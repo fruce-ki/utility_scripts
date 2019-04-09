558fc1eb362b26f09e1c192d98476df091a8a2a1
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-04-09 17:39:23 +0200

    smarter grouping

--- nonTargetGuides2controlGenes.R
+++ nonTargetGuides2controlGenes.R
@@ -7,11 +7,13 @@ spec = matrix(c(
   'countsFile'    , 'f', 1, "character", "Tab-separated table of counts.",
   'groupCol'      , 'g', 2, "character", "Name of the column containing the group names ('group').",
   'help'          , 'h', 0, "logical"  , "Help.",
+  'mincount'      , 'm', 2, "numeric"  , "Minimum count to separate control guides into detected and non (1).",
   'guidesPerGene' , 'n', 2, "numeric"  , "Desired number of guides per control \"gene\" (6).",
   'outFile'       , 'o', 2, "character", "Output file (overwrite input file).",
   'nonrandom'     , 'r', 0, "logical"  , "Assign guides to groups in order of appearance instead of randomly (FALSE).",
   'seed'          , 's', 2, "numeric"  , "Seed for reproducible pseudo-randomisation.",
-  'targetCol'     , 't', 2, "character", "Name of the column containing the guide names ('id')."
+  'targetCol'     , 't', 2, "character", "Name of the column containing the guide names ('id').",
+  'reference'     , 'z', 2, "character", "Comma separated column names across which to apply mincount for the controls. (if NULL, then all)"
 ), byrow=TRUE, ncol=5)
 opt = getopt(spec)
 
@@ -20,12 +22,14 @@ if ( !is.null(opt$help) ) {
   q(status=1)
 }
 
-if ( is.null(opt$guidesPerGene) )  { opt$guidesPerGene <- 6L }
-if ( is.null(opt$outFile) )        { opt$outFile <- opt$countsFile }
-if ( is.null(opt$targetCol) )      { opt$targetCol <- 'id' }
-if ( is.null(opt$groupCol) )       { opt$groupCol <- 'group' }
-if ( !is.null(opt$seed) )          { set.seed(opt$seed) }
-if ( is.null(opt$nonrandom) )      { opt$nonrandom <- FALSE } else { opt$randomise <- TRUE }
+if ( is.null(opt$guidesPerGene) ) { opt$guidesPerGene <- 6L }
+if ( is.null(opt$outFile) )       { opt$outFile <- opt$countsFile }
+if ( is.null(opt$targetCol) )     { opt$targetCol <- 'id' }
+if ( is.null(opt$groupCol) )      { opt$groupCol <- 'group' }
+if (! is.null(opt$seed) )         { set.seed(opt$seed) }
+if ( is.null(opt$nonrandom) )     { opt$nonrandom <- FALSE } else { opt$randomise <- TRUE }
+if ( is.null(opt$mincount) )      { opt$mincount <- 1 }
+if (! is.null(opt$reference) )    { opt$reference <- unlist(strsplit(opt$reference, ',')) }
 
 # Group names
 groupNames <- unlist(strsplit(opt$controlGroups, ',', fixed=TRUE))
@@ -40,15 +44,54 @@ names(dt)[which(names(dt)==opt$groupCol)] <- 'sexygroup666'
 names(dt)[which(names(dt)==opt$targetCol)] <- 'sexyid666'
 
 for (gr in groupNames){
-  # Group separately guides that are present andguides that are not present.
-  nonzero <- rowSums(dt[, -c(1,2)]) > 0
-  # Relevant rows
-  sel <- dt$sexygroup666 == gr
+  # All guides above the threshold.
+  meanCount <- NULL
+  if (is.null(opt$reference)) {
+    # All samples == columns excluding the guide and group ids.
+    meanCount <- rowMeans(dt[, -c(grpidx, tgtidx), with=FALSE])
+  } else {
+    # Specified samples only.
+    meanCount <- rowMeans(dt[, c(opt$reference), with=FALSE])
+  }
+  
+  aux <- data.table(id = dt$sexyid666,
+                    grp = dt$sexygroup666,
+                    cnt = meanCount,
+                    nonzero = meanCount >= opt$mincount,
+                    sel = dt$sexygroup666 == gr
+          )[order(cnt)]
+  aux <- aux[(sel), ]
+
   # Number of guides
-  nx <- sum(sel & nonzero)
-  nz <- sum(sel & !nonzero)
+  nx <- nrow(aux[(nonzero),])
+  nz <- nrow(aux[(!nonzero),])
+  # Shift a few guides around to ensure at least the
+  # above-threshold group is divisible by the number of guides per gene.
+  remainder <- nx %% opt$guidesPerGene
+  if (remainder != 0){
+    if ((nx - remainder > opt$guidesPerGene && 
+         remainder <= opt$guidesPerGene / 2) ||
+        nz < opt$guidesPerGene - remainder) {
+      # If there are enough guides for at least one above-threshold group, 
+      # and the remainder is not big enough to justify rounding up, 
+      # or if there wouldn't be enough below-threshold guides to substract from,
+      # reclassify the `remainder` bottom-count guides that are above threshold into below-threshold.
+      aux[(nz + 1):(nz + remainder), nonzero := FALSE] 
+      nx <- nx - remainder
+      nz <- nz + remainder
+      
+    } else {
+      difference <- opt$guidesPerGene - remainder
+      # Reclassify the `difference` top-count guides that are below threshold into above-threshold.
+      aux[(nz - difference + 1):nz, nonzero := TRUE] 
+      # Round up.
+      nx <- nx + difference
+      nz <- nz - difference
+    }
+  }
+  
   # Number of subgroups to split into, decimals rounded up. The last group will have fewer guides if division was not whole.
-  splitX <- floor(nx / opt$guidesPerGene) + 1
+  splitX <- floor(nx / opt$guidesPerGene) + 1  # By now nx should be divisible, but allow freak case of nx < guidesPerGene.
   splitZ <- floor(nz / opt$guidesPerGene) + 1
   # Create new groupNames of suffixes
   newGroupsX <- paste0(gr, '_X_', rep(1:splitX, each = opt$guidesPerGene))
@@ -59,11 +102,15 @@ for (gr in groupNames){
   # Shuffle for good measure
   if (!opt$nonrandom) {
     newGroupsX <- sample(newGroupsX)
-    newGroupsZ <- sample(newGroupsZ)
+    # newGroupsZ <- sample(newGroupsZ)
   }
+  aux[(nonzero), grp := newGroupsX]
+  aux[(!nonzero), grp := newGroupsZ]
+  
   # And put the new values back into the table
-  dt[sel & nonzero, sexygroup666 := newGroupsX]
-  dt[sel & !nonzero, sexygroup666 := newGroupsZ]
+  setkey(aux, id)
+  setkey(dt, sexyid666)
+  dt[aux$id, sexygroup666 := aux$grp]
 }
 
 # Restore original column names
