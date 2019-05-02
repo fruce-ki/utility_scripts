e761ca96c8a8433e89e05bf94deb4951d66b939d
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-05-02 20:00:25 +0200

    fix

--- mageck_workflow.sh
+++ mageck_workflow.sh
@@ -9,15 +9,16 @@
 #SBATCH --output=mageck-wf.out
 #SBATCH --error=mageck-wf.err
 
+set -e
+
 ## Parameters ##
 function usage() {
     echo "Usage:"
     echo "      $0 -i INDIR -l GUIDES_LIB -b DEMUX_TABLE -n COUNTS_OUTDIR -c CONTRASTS_TABLE -m MAGECK_OUTDIR
                 [-r REVCOMP] [-p PAD_BASE] [-s SPACER_LENGTH] [-u UMI_LENGTH] [-d DEMUX_BC_LENGTH] [-M BC_MISMATCHES] [-z MINCOUNT] [-E ENTREZ_FIELD]
-                [-Z SAMPLES -C CTRL_GUIDE_GRPS -G GUIDES_PER_GENE]"
+                [-Z REF_SAMPLES -C CTRL_GUIDE_GRPS -G GUIDES_PER_GENE]"
 		exit 1
 }
-
 # Defaults
 pad="C"
 countthresh=50
@@ -40,7 +41,7 @@ while getopts 'i:l:b:r:n:c:m:r:C:G:z:Z:p:s:u:d:M:E' flag; do
     C) ctrlguides="${OPTARG}" ;;      # Comma-separated list of control guide group names.
     G) guidespergene="${OPTARG}" ;;   # Guides per gene
     z) countthresh="${OPTARG}" ;;     # Minimum read count per guide (50)
-    Z) countthresh="${OPTARG}" ;;     # Comma-separated sample-names to apply the counts threshold for control guide purposes
+    Z) refSamps="${OPTARG}" ;;        # Comma-separated sample-names to apply the counts threshold for control guide purposes
     p) pad="${OPTARG}" ;;             # Padding base ("C")
     s) spacer="${OPTARG}" ;;          # Spacer length (20)
     u) umi="${OPTARG}" ;;             # UMI length (6)
@@ -69,7 +70,7 @@ fi
 echo "Pre-process to BAM to FASTQ, align, and count."
 nextflow run zuberlab/crispr-process-nf $revcomp --inputDir $indir --library $library --padding_base $pad --spacer_length $spacer --barcode_demux_length $demux --barcode_random_length $umi --barcode_demux_mismatches $bcmm --barcodes $barcodes --outputDir $countsdir -profile ii2 --resume
 ld=$(basename $library)
-counts="${countsdir}/counts/${ld/.txt/''}/counts_mageck.txt"
+counts="${countsdir}/counts/${ld/.txt/}/counts_mageck.txt"
 
 # echo "Merge some columns, add some columns."
 # Rscript ~/utility_scripts/sum_cols.R ./process/crispr_nf/counts/library/counts_mageck.txt ${counts/.txt/_merged.txt} 'NoIFNG_d0,NoIFNG_d0_3,NoIFNG_d0_4' NoIFNG_d0 TRUE TRUE
@@ -77,53 +78,69 @@ counts="${countsdir}/counts/${ld/.txt/''}/counts_mageck.txt"
 # fileutilities.py T ./process/crispr_nf/counts/library/counts_d0-merged.txt ./data/counts_plasmid-pool.txt -r -i --appnd > ${counts/.txt/_added.txt}
 # $counts=${counts/.txt/_added.txt}
 
+echo ''
 echo "Group control guides into genes."
 if ! [ -z "$ctrlguides" ]; then
-  srun Rscript ~/utility_scripts/nonTargetGuides2controlGenes.R -c $ctrlguides -f $counts -o ${counts/.txt/_ctrls-grouped.txt} -n $guidespergene -g group -t id -m $countthresh -z 'NoIFNG_d0,imc_surface_plasmid'
+  srun Rscript ~/utility_scripts/nonTargetGuides2controlGenes.R -c $ctrlguides -f $counts -o ${counts/.txt/_ctrls-grouped.txt} -n $guidespergene -g group -t id -m $countthresh -z $refSamps
   counts="${counts/.txt/_ctrls-grouped.txt}"
 else
   ctrlguides="hakunamatata_dummy" # dummy value that will not match patterns later on
 fi
 
+echo ''
 echo "Extract entrez IDs from sgRNA IDs."
 lib="$(dirname $library)"
-srun cut -f 1 $library | perl -e '$pat = join "|", split "," $ARGV[0]; while($line=<STDIN>){ if ($line=~/(?<!\w)id(?!\w)/) { print "Entrez\n" } elsif ( $line=~/^($pat)/ ){ print "$1\n" } else { @a = split(/_/, $line); print "$a[$ARGV[2]]\n" }}' $ctrlguides $entrezfield > ${lib}/entrez.txt
+srun cut -f 1 $library | perl -e '$pat = join "|", split ",", $ARGV[0]; while($line=<STDIN>){ if ($line=~/(?<!\w)id(?!\w)/) { print "Entrez\n" } elsif ( $line=~/^($pat)/ ){ print "$1\n" } else { @a = split(/_/, $line); print "$a[$ARGV[2]]\n" }}' $ctrlguides $entrezfield > ${lib}/entrez.txt
 srun fileutilities.py T $library ${lib}/entrez.txt --appnd outer -r > ${library}_entrez.txt
 library=${library}_entrez.txt
 
+echo ''
 echo "MAGECK."
 nextflow run zuberlab/crispr-mageck-nf --contrasts $contrasts --counts $counts --outputDir $mageckdir --min_count $countthresh -profile ii2 --legacy --resume
 
+echo ''
 echo "Rename columns."
 renamed="/renamed"
 srun fileutilities.py T $mageckdir --dir | fileutilities.py P --loop S sh ~/utility_scripts/rename_mageck_columns.sh {abs} {abs}${renamed}
 
+echo ''
 echo "Merge all gene-level outputs."
 genes="${mageckdir}/genes_all.tsv"
 srun --mem=10000 fileutilities.py T ${mageckdir}/*${renamed}/genes_pos_stats.txt ${mageckdir}/*${renamed}/genes_neg_stats.txt -r -i --appnd outer > $genes
 
+echo ''
 echo "Merge all guide-level outputs and clean up redundant columns."
 guides="${mageckdir}/guides_all.tsv"
 srun --mem=10000 fileutilities.py T $library ${mageckdir}/*${renamed}/guides_stats.txt -r -i --appnd outer > $guides
 srun --mem=10000 fileutilities.py T $guides -r --mrgdups $(head -n1 $guides | fileutilities.py D --swap "\n" | perl -e '$i=0; while($field = <STDIN>){print "$i " if $field=~/group/; $i++} print "\n";') > ${guides/.tsv/_dedup.tsv}
 guides="${guides/.tsv/_dedup.tsv}"
-nc=$(fileutilities.py T $guides --cntcols)
+nc=$(perl -e '$ARGV[0] =~/^(\d+)/; print $1' $(fileutilities.py T $guides --cntcols))
 srun --mem=10000 fileutilities.py T $guides -r --cols $(expr $nc - 1) $(expr $nc - 2) 0 $(expr $nc - 3) 1:$(expr $nc - 4) > ${guides/.tsv/_reord.tsv}
 guides="${guides/.tsv/_reord.tsv}"
 
+echo ''
 echo "Merge guide-level and gene-level outputs and clean up redundant columns."
 out="${mageckdir}/guides+genes.tsv"
 srun --mem=10000 fileutilities.py T $guides $genes -r --merge outer yes no > $out
 srun --mem=10000 fileutilities.py T $out -r --mrgdups $(head -n1 $out | fileutilities.py D --swap "\n" | perl -e '$i=0; while($field = <STDIN>){print "$i " if $field=~/group/; $i++}') > ${out/.tsv/_dedup.tsv}
 out="${out/.tsv/_dedup.tsv}"
-nc=$(fileutilities.py T $out --cntcols)
+nc=$(perl -e '$ARGV[0] =~/^(\d+)/; print $1' $(fileutilities.py T $out --cntcols))
 srun --mem=10000 fileutilities.py T $out -r --cols $(expr $nc - 1) 0:$(expr $nc - 2) > ${out/.tsv/_reord.tsv}
 out="${out/.tsv/_reord.tsv}"
 
+echo ''
 echo "Add -log10(p)."
-l10="${mageckdir}/tmp_log10p.tsv"
-Rscript ~/utility_scripts/p2log10p.R $out $l10
-srun --mem=10000 fileutilities T $out $l10 -i -r --appnd outer > ${out/.tsv/_l10p.tsv}
-#rm l10
+srun --mem=10000 add_log10p.R $out ${out/.tsv/_l10p.tsv}
+out=${out/.tsv/_l10p.tsv}
+
+echo ''
+echo "Cleaning up intermediate files."
+rm ${mageckdir}/genes_all.tsv
+rm ${mageckdir}/guides_all.tsv
+rm ${mageckdir}/guides_all_dedup.tsv
+rm ${mageckdir}/guides_all_dedup_reord.tsv
+rm ${mageckdir}/guides+genes.tsv
+rm ${mageckdir}/guides+genes_dedup.tsv
+rm ${mageckdir}/guides+genes_dedup_reord.tsv
 
 echo "All done!"
