c6c784d9996bc738c947d47d6843e7c68d50d9de
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-05-06 14:37:02 +0200

    named arguments instead of positional

--- add_log10p.R
+++ add_log10p.R
@@ -4,35 +4,39 @@
 # compute their -log10(p)
 # and output to a new file, indexed by 'id'.
 
-args <- commandArgs(trailingOnly = TRUE)
-infile <- args[1]  	# tsv table
-outfile <- args[2] 	# output table for calc'ed values
-ppat <- args[3]    	# pattern to recognise pvalue column names
-id <- args[4]      	# name of index column
+library(getopt)
 
-if (is.na(ppat))
-	ppat <- '\\.p\\.'
-if(is.na(id))
-	id <- 'id'
+spec = matrix(c(
+  'infile'        , 'i', 1, "character", "Input .tsv",
+  'outfile'       , 'o', 1, "character", "Output .tsv",
+  'ppat'          , 'p', 1, "character", "Pattern to recognise pvalue column names",
+  'id'            , 'r', 1, "character", "Column name for row IDs"
+), byrow=TRUE, ncol=5)
+opt = getopt(spec)
+
+if (is.null(opt$ppat))
+  opt$ppat <- '\\.p\\.'
+if(is.null(opt$id))
+  opt$id <- 'id'
 
 
 library(data.table)
 
-df <- fread(infile, sep="\t", header=TRUE)
+df <- fread(opt$infile, sep="\t", header=TRUE)
 
 
 # Identify and isolate id and pvalue-related columns.
-sel <- (grepl(ppat, names(df), perl=TRUE) |
-          grepl(paste0("^",id,"$"), names(df), perl=TRUE))
+sel <- (grepl(opt$ppat, names(df), perl=TRUE) |
+          grepl(paste0("^",opt$id,"$"), names(df), perl=TRUE))
 subdf <- df[, sel, with=FALSE]
 
 # Transform
-df2 = data.table(id = subdf[[id]])
-for (n in names(subdf[, -c(id), with=FALSE])) {
+df2 <- data.table(id = subdf[[opt$id]])
+for (n in names(subdf[, -c(opt$id), with=FALSE])) {
   df2[[n]] = -log10(as.numeric(subdf[[n]]))
 }
 # Rename
-names(df2) <- gsub(ppat, ".-Log10p.", names(df2), perl=TRUE)
+names(df2) <- gsub(opt$ppat, ".-Log10p.", names(df2), perl=TRUE)
 
-fwrite(merge(df, df2, by=id, all.x=TRUE),
-			file=outfile, sep="\t", col.names=TRUE, row.names=FALSE)
+fwrite(merge(df, df2, by.x=opt$id, by.y="id", all.x=TRUE),
+			file=opt$outfile, sep="\t", col.names=TRUE, row.names=FALSE)
