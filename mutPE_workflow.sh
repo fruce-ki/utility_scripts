7295b982df823930f1c0053bafd35751ab506952
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-05-10 10:34:12 +0200

    forgot to uncomment all the commands

--- mutPE_workflow.sh
+++ mutPE_workflow.sh
@@ -43,34 +43,34 @@ fi
 
 if [[ -z $renamefq ]]; then
 	echo "Renaming & compressing .fq to .fastq.gz."
-	#fileutilities.py T ${base}/${data}/${run} --dir fq | fileutilities.py P --loop S mv {abs} {dir}/{bas}.fastq \; srun gzip {dir}/{bas}.fastq
+	fileutilities.py T ${base}/${data}/${run} --dir fq | fileutilities.py P --loop S mv {abs} {dir}/{bas}.fastq \; srun gzip {dir}/{bas}.fastq
 fi
 
 if [[ -z $issra ]]; then
 	echo "Converting .sra to fastq.gz."
-	#fileutilities.py T ${base}/${data}/${run} --dir .sra | fileutilities.py P --loop S srun ~/utility_scripts/sra2fastq.sh ${base}/${data}/${run} ${base}/${data}/${run}/{val}
+	fileutilities.py T ${base}/${data}/${run} --dir .sra | fileutilities.py P --loop S srun ~/utility_scripts/sra2fastq.sh ${base}/${data}/${run} ${base}/${data}/${run}/{val}
 fi
 
 echo "Merging pairs."
-#fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop S srun flash ,-t 1 ,-M 150 ,-x 0.25 ,-d ${base}/${process}/${run} ,-o {ali}_x25 ,-z ${base}/${data}/${run}/{ali}_1.fastq.gz ${base}/${data}/${run}/{ali}_2.fastq.gz
+fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop S srun flash ,-t 1 ,-M 150 ,-x 0.25 ,-d ${base}/${process}/${run} ,-o {ali}_x25 ,-z ${base}/${data}/${run}/{ali}_1.fastq.gz ${base}/${data}/${run}/{ali}_2.fastq.gz
 
 echo "Aligning."
-#fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop S srun ,-c 4 ~/utility_scripts/bowtie2aln.sh ${base}/${aux}/${bowtie2idx} ${base}/${process}/${run} ${base}/${process}/${run} {ali}_x25.extendedFrags false 4 ,--very-sensitive-local
+fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop S srun ,-c 4 ~/utility_scripts/bowtie2aln.sh ${base}/${aux}/${bowtie2idx} ${base}/${process}/${run} ${base}/${process}/${run} {ali}_x25.extendedFrags false 4 ,--very-sensitive-local
 
 
 echo "Stratifying BAMs by number of mismatches."
-#fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 1 2
-#fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 3 10
-#fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 11 30
+fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 1 2
+fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 3 10
+fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 11 30
 
 
 echo "Piling up."
-#fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.bam$' | fileutilities.py P --loop S srun samtools mpileup ,-A ,-B ,-f ${base}/${aux}/${bowtie2idx}.fa ,-d 1000000000 {abs} \> {dir}/{bas}.pileup
+fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.bam$' | fileutilities.py P --loop S srun samtools mpileup ,-A ,-B ,-f ${base}/${aux}/${bowtie2idx}.fa ,-d 1000000000 {abs} \> {dir}/{bas}.pileup
 
 echo "Counting."
-#fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.pileup$' | fileutilities.py P --loop S python3 ./analysis/MutPE_quantification.py ,-p {abs} \> {dir}/{bas}.stats
+fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.pileup$' | fileutilities.py P --loop S python3 ./analysis/MutPE_quantification.py ,-p {abs} \> {dir}/{bas}.stats
 
 echo "Visualising."
-mutPE_mutation-stats_viz.R ${base}/${results}/${run} ${run} NULL ${base}/${process}/${run}/*.stats
+mutPE_mutation-stats_viz.R ${base}/${results}/${run} ${run} NULL yes ${base}/${process}/${run}/*.stats
 
 echo "Finished."
