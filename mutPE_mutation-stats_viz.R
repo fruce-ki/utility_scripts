484ce4010b008e09c66376328de1c9009cbc408f
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-05-10 13:07:53 +0200

    bugfix

--- mutPE_mutation-stats_viz.R
+++ mutPE_mutation-stats_viz.R
@@ -10,8 +10,7 @@ outdir <- args[1]
 prefix <- args[2]             # for the collective PDF and HTML output files
 ymax <- args[3]               # "NULL" for automatic
 collectiveonly <- args[4]       # 'yes'/'no' useful to prevent file spam when many stats files are input
-statsfiles <- args[4:length(args)]       # TSV input files: seq \t pos \t type \t count \t depth
-# statsfiles <- '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/mutpe/process/B18_GCB/B18_GCB_KO_x25.extendedFrags_sorted_3-10.stats'
+statsfiles <- args[5:length(args)]       # TSV input files: seq \t pos \t type \t count \t depth
 
 library(tidyverse)
 library(ggrepel)
@@ -33,7 +32,7 @@ pdf(file.path(outdir, paste0(prefix, '.pdf')))
 for (sf in statsfiles){
   # Input
   posdata <- read_tsv(sf) 
-  if (! all(names(posdata) == c('amplicon', 'pos', 'type', 'freq', 'depth')))
+  if (all(names(posdata) == c('amplicon', 'pos', 'type', 'freq', 'total')))
     names(posdata) <- c('seq', 'pos', 'type', 'count', 'depth')
   stopifnot(all(names(posdata) == c('seq', 'pos', 'type', 'count', 'depth')))
   
@@ -54,7 +53,7 @@ for (sf in statsfiles){
   
   # Plot number of positions per mutation type
   print(posdata %>%
-    filter((mutated)) %>%
+    filter(mutated) %>%
     group_by(seq, type) %>%
     summarise(frequency = sum(count>0)) %>%
     ggplot(aes(x = type, y = frequency, fill=type)) + 
@@ -66,7 +65,7 @@ for (sf in statsfiles){
   
   # Plot number of reads per mutation type
   print(posdata %>%
-    filter((mutated)) %>%
+    filter(mutated) %>%
     group_by(seq, type) %>%
     summarise(frequency = sum(count)) %>%
     ggplot(aes(x = type, y = frequency, fill=type)) + 
@@ -102,10 +101,10 @@ for (sf in statsfiles){
                        unique() %>%
                        filter(aggrfreq >= quantile(posdata$aggrfreq, 0.95)),
                      aes(y=aggrfreq, label=pos), min.segment.length = 0, label.size = 0, fill="transparent", force=1) +
-      scale_y_continuous('Frequency (bars)', sec.axis = sec_axis(trans= ~ . * maxdepth / maxfreq / 1.2, name = "Coverage (line)"), limits = c(0, 1)) +
+      scale_y_continuous('Frequency (bars)', sec.axis = sec_axis(trans= ~ . * maxdepth / maxfreq / 1.2, name = "Coverage (line)")) +
       scale_fill_gradient(low='blue', high='magenta') +
       scale_x_continuous('Position', limits = c(minpos, maxpos)) +
-      coord_cartesian(ylim=c(0, 1.1 * maxfreq)) +
+      # coord_cartesian(ylim=c(NULL, 1.1 * maxfreq)) +
       ggtitle("Fraction of reads with mutation at each position.", subtitle=basename(sf)) +
       theme(legend.position = 'none', 
             panel.background = element_rect(fill="white"), 
@@ -126,7 +125,7 @@ for (sf in statsfiles){
         geom_bar(aes(fill=type), stat = 'identity', position=position_stack()) +
         scale_x_continuous('Position', limits = c(minpos, maxpos)) +
         ylab("Frequency") +
-        coord_cartesian(ylim=c(0, 1.1 * maxfreq)) +
+        # coord_cartesian(ylim=c(NULL, 1.1 * maxfreq)) +
         ggtitle("Fraction of reads with mutation at each position.", subtitle=basename(sf)) +
         guides(ncol=4, by.row=TRUE) +
         theme(panel.background = element_rect(fill="white"), 
@@ -153,7 +152,7 @@ htmlwidgets::saveWidget(ggplotly(
       facet_wrap( . ~ seq) +
       geom_bar(stat = 'identity', position=position_identity(), alpha=0.3) +
       scale_x_continuous("Position", limits = c(minpos, maxpos)) +
-      scale_y_continuous('Coverage', limits = c(0, NA)) +
+      scale_y_continuous('Frequency') +
       ggtitle("Fraction of reads with mutation at each position.") +
       theme(panel.background = element_rect(fill="white"), 
             panel.grid.major.y = element_line(colour='grey95'),
@@ -172,7 +171,7 @@ htmlwidgets::saveWidget(ggplotly(
     facet_wrap( . ~ seq) +
     geom_line() +
     scale_x_continuous('Position', limits = c(minpos, maxpos)) +
-    scale_y_continuous('Coverage', limits = c(0, NA)) +
+    scale_y_continuous('Coverage') +
     ggtitle("Fraction of reads with mutation at each position.") +
     theme(legend.position = 'bottom', 
           panel.background = element_rect(fill="white"), 
