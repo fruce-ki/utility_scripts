10610cae3852fb7213ed245852435f34f0b25b4d
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-05-28 14:15:48 +0200

    added table outputs

--- mutPE_workflow.sh
+++ mutPE_workflow.sh
@@ -32,48 +32,49 @@ while getopts 'd:D:b:p:r:a:i:sf' flag; do
 done
 
 
-echo "Creating destinations."
 if [[ ! -d "${base}/${process}/${run}" ]]; then
 	mkdir -p "${base}/${process}/${run}"
+  echo "Created destination: ${base}/${process}/${run}"
 fi
 if [[ ! -d "${base}/${results}/${run}" ]]; then
 	mkdir -p "${base}/${results}/${run}"
+  echo "Created destination: ${base}/${results}/${run}"
 fi
 
 
-if [[ -z $renamefq ]]; then
-	echo "Renaming & compressing .fq to .fastq.gz."
-	fileutilities.py T ${base}/${data}/${run} --dir fq | fileutilities.py P --loop S mv {abs} {dir}/{bas}.fastq \; srun gzip {dir}/{bas}.fastq
+if [[ $renamefq ]]; then
+	echo "Renaming & compressing .fq to .fastq.gz"
+	fileutilities.py T ${base}/${data}/${run} --dir fq | fileutilities.py P --loop mv {abs} {dir}/{bas}.fastq \; srun gzip {dir}/{bas}.fastq
 fi
 
-if [[ -z $issra ]]; then
-	echo "Converting .sra to fastq.gz."
-	fileutilities.py T ${base}/${data}/${run} --dir .sra | fileutilities.py P --loop S srun ~/utility_scripts/sra2fastq.sh ${base}/${data}/${run} ${base}/${data}/${run}/{val}
+if [[ $issra ]]; then
+	echo "Converting ${base}/${data}/${run}/*.sra to ${base}/${data}/${run}/*.fastq.gz"
+	fileutilities.py T ${base}/${data}/${run} --dir .sra | fileutilities.py P --loop srun ~/utility_scripts/sra2fastq.sh ${base}/${data}/${run} ${base}/${data}/${run}/{val}
 fi
 
-echo "Extracting to FASTQ."
+echo "Extracting ${base}/${data}/${run}/*.bam to ${base}/${data}/${run}/*_1/2.fastq.gz"
 fileutilities.py T ${base}/${data}/${run} --dir .bam | fileutilities.py P --loop samtools fastq ,-c 1 ,-1 {dir}/{ali}_1.fastq.gz ,-2 {dir}/{ali}_2.fastq.gz {abs}
 
-echo "Merging pairs."
-fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop S srun flash ,-t 1 ,-M 150 ,-x 0.25 ,-d ${base}/${process}/${run} ,-o {ali}_x25 ,-z ${base}/${data}/${run}/{ali}_1.fastq.gz ${base}/${data}/${run}/{ali}_2.fastq.gz
+echo "Merging pairs in ${base}/${data}/${run} to ${base}/${process}/${run}/*_x25..fastq.gz"
+fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop srun flash ,-t 1 ,-M 150 ,-x 0.25 ,-d ${base}/${process}/${run} ,-o {ali}_x25 ,-z ${base}/${data}/${run}/{ali}_1.fastq.gz ${base}/${data}/${run}/{ali}_2.fastq.gz
 
-echo "Aligning."
-fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop S srun ,-c 4 ~/utility_scripts/bowtie2aln.sh ${base}/${aux}/${bowtie2idx} ${base}/${process}/${run} ${base}/${process}/${run} {ali}_x25.extendedFrags false 4 ,--very-sensitive-local
-
-
-echo "Stratifying BAMs by number of mismatches."
-fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 1 2
-fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 3 10
-fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop S srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 11 30
+echo "Aligning ${base}/${data}/${run}/*.fastq.gz to ${base}/${aux}/${bowtie2idx} and outputting to ${base}/${process}/${run}/*_x25.extendedFrags.bam"
+fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop srun ,-c 4 ~/utility_scripts/bowtie2aln.sh ${base}/${aux}/${bowtie2idx} ${base}/${process}/${run} ${base}/${process}/${run} {ali}_x25.extendedFrags false 4 ,--very-sensitive-local
 
 
-echo "Piling up."
-fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.bam$' | fileutilities.py P --loop S srun samtools mpileup ,-A ,-B ,-f ${base}/${aux}/${bowtie2idx}.fa ,-d 1000000000 {abs} \> {dir}/{bas}.pileup
+echo "Stratifying ${base}/${process}/${run}/*.sorted.bam by number of mismatches"
+fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 1 2
+fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 3 10
+fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 11 30
+
+
+echo "Piling up ${base}/${process}/${run}/\d+-\d+.bam onto ${base}/${aux}/${bowtie2idx}.fa"
+fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.bam$' | fileutilities.py P --loop srun samtools mpileup ,-A ,-B ,-f ${base}/${aux}/${bowtie2idx}.fa ,-d 1000000000 {abs} \> {dir}/{bas}.pileup
 
 echo "Counting."
-fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.pileup$' | fileutilities.py P --loop S python3 ./analysis/MutPE_quantification.py ,-p {abs} \> {dir}/{bas}.stats
+fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.pileup$' | fileutilities.py P --loop python3 ./analysis/MutPE_quantification.py ,-p {abs} \> {dir}/{bas}.stats
 
-echo "Visualising."
-mutPE_mutation-stats_viz.R ${base}/${results}/${run} ${run} NULL yes ${base}/${process}/${run}/*.stats
+echo "Visualising ${base}/${process}/${run}/*.stats into ${base}/${results}/${run}/${run/\//_}*.html/pdf"
+mutPE_mutation-stats_viz.R ${base}/${results}/${run} ${run/\//_} NULL yes ${base}/${process}/${run}/*.stats
 
 echo "Finished."
