c16a3884cf13c440d2994bdd3333adfb25b740af
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-06-04 17:22:13 +0200

    reflect new syntax

--- bam_barcode_diagnostic.sh
+++ bam_barcode_diagnostic.sh
@@ -3,11 +3,12 @@
 set -e
 
 source ~/miniconda3/bin/activate mybasics
-module load samtools/1.9-foss-2017a
+#module load samtools/1.9-foss-2017a
+#module load pysam/0.15.1-foss-2017a-python-3.6.4
 
 in=$1
 out=$2
 pattern='TTCCAGCATAGCTCTTAAAC'
 
-samtools view $in | sequtilities.py D --samPatternStats $pattern ${#pattern} N \-4 4 > ${out}
+sequtilities.py T $in --samPatternStats $pattern ${#pattern} N \-4 4 > $out
 bam_barcode_report.R $out
