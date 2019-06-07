28f4754cb743e0bf44b2462b8a56577d79b62e26
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-06-07 11:29:28 +0200

    Update bowtie2aln.sh

--- bowtie2aln.sh
+++ bowtie2aln.sh
@@ -11,13 +11,16 @@ paired=$1 ; shift
 threads=$1 ; shift
 params=( "$@" )
 
+if [ ! -d "$outdir" ]; then
+	mkdir -p $outdir
+fi
 
 if [ $paired == 'false' ]; then
 	# single end
-	bowtie2 -x $idx  -U ${indir}/${pref}.fastq.gz -p $threads $params | samtools view -Sb | samtools sort -n -@ $threads -O BAM -o ${outdir}/${pref}.bam
+	bowtie2 -x $idx  -U ${indir}/${pref}.fastq.gz -p $threads $(expr $params / 2) | samtools view -Sb | samtools sort -n -@ $(expr $threads / 2) -O BAM -o ${outdir}/${pref}.bam
 else
 	# paired end
-	bowtie2 -x $idx  -1 ${indir}/${pref}_1.fastq.gz -2 ${indir}/${pref}_2.fastq.gz -p $threads $params | samtools view -Sb | samtools sort -n -@ $threads -O BAM -o ${outdir}/${pref}.bam
+	bowtie2 -x $idx  -1 ${indir}/${pref}_1.fastq.gz -2 ${indir}/${pref}_2.fastq.gz -p $(expr $threads / 2) $params | samtools view -Sb | samtools sort -n -@ $(expr $threads / 2) -O BAM -o ${outdir}/${pref}.bam
 fi
 
 samtools sort -@ $threads -o ${outdir}/${pref}_sorted.bam ${outdir}/${pref}.bam
