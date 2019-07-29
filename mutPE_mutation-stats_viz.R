4b7b0d91ff90ea4f776f50a04eff636e5a4f1a01
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-07-29 17:59:10 +0200

    update

--- mutPE_mutation-stats_viz.R
+++ mutPE_mutation-stats_viz.R
@@ -6,10 +6,15 @@
 # - read coverage interactively overlayed for all the input files
 
 ## REQUIRES: pandoc
+library(data.table)
+library(dtplyr)
+library(ggplot2)
+library(ggrepel)
+library(plotly)
 
 #args <- c('~/', 'test', 'NULL', 'no', 'HDR1:280:6,HDR2:280:6', '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/round5/process/in-vivo/HDR2/86754_B18_C2_G_e1_x25.extendedFrags_sorted_3-10.stats')
 #args <- c('~/', 'test', 'NULL', 'no', '-:0:0', '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/round5/process/in-vivo/HDRc/86752_B18_Cc_G_e1_x25.extendedFrags_sorted_1-2.stats')
-#args <- c('~/', 'test', 'NULL', 'no', '-:0:0', '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/yeap2015/process/SRS1052796/SRR2229664_x25.extendedFrags_sorted_3-10.stats')
+#args <- c('~/', 'test', 'NULL', 'no', '-:0:0', '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/yeap2015/process/pro/SRS1052796/SRR2229665_x25.extendedFrags_sorted_1-2.stats', '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/yeap2015/process/pro/SRS1052796/SRR2229665_x25.extendedFrags_sorted_3-10.stats', '/Volumes/groups/pavri/Kimon/ursi/mutPEseq/yeap2015/process/pro/SRS1052796/SRR2229665_x25.extendedFrags_sorted11-30.stats')
 args <- commandArgs(trailingOnly = TRUE)
 outdir <- args[1]
 prefix <- args[2]             # for the collective PDF and HTML output files
@@ -20,10 +25,6 @@ trimmedend <- 19              # shorten reference name. Keep to here
 offsets <- args[5]            # Correct for deletions in references. "REF:START:LENGTH,REF:START:LENGTH" ie. "HDR2:280:6"
                               # For no corrections needed you can use "-:0:0". REF will be grepl'ed.
 statsfiles <- args[6:length(args)]       # TSV input files: seq \t pos \t type \t count \t depth
-
-library(tidyverse)
-library(ggrepel)
-library(plotly)
 
 # Expand the corrections string.
 offsets <- strsplit(offsets, ',', fixed=TRUE)[[1]]
@@ -45,7 +46,7 @@ pdf(file.path(outdir, paste0(prefix, '.pdf')))
 # sf <- statsfiles[1]
 for (sf in statsfiles){
   # Input
-  posdata <- read_tsv(sf)
+  posdata <- fread(sf)
   if (dim(posdata)[1]==0) {
     warning(paste("No content found in ", sf))
     next
@@ -57,47 +58,42 @@ for (sf in statsfiles){
   # Correct for reference deletions
   for (ref in 1:dim(offsets)[1]) {
     #ref <- 1
-    sel <- (grepl(offsets[[ref,1]], posdata$seq) & posdata$pos >= as.integer(offsets[[ref,2]]))
+    sel <- ( grepl(offsets[[ref,1]], posdata$seq) & 
+             posdata$pos >= as.integer(offsets[[ref,2]]) )
     posdata$pos[sel] <- posdata$pos[sel] + as.integer(offsets[[ref,3]])
   }
 
   # Shorten the reference name so it fits in the facet bars.
-  posdata <- posdata %>%
-    mutate(seq = substr(seq, trimmedstart, trimmedend))
-
-  # Calculate frequencies and aggregate frequencies by position
-  posdata <- posdata %>%
-    mutate(freq = count / depth, mutated = freq != 0) %>%
-    group_by(seq, pos) %>%
-    mutate(aggrfreq = sum(freq)) %>%
-    ungroup()
+  posdata[, seq := substr(seq, trimmedstart, trimmedend) ]
 
   # Create interruptions for coordinates without info.
   missing <- which(! 1:max(posdata$pos) %in% unique(posdata$pos))  # Maybe there is more info missing than just the deletions?
   if(length(missing) > 0){
     posdata <- rbind(posdata,
-                     data.frame(seq=posdata$seq[1],      # A single .stats file should contain one sample, mapped to one reference.
+                     data.frame(seq=posdata$seq[1],      # Assumes a single reference per .stats file.
                                 pos=missing,
                                 type='ref_indel',
                                 count=0,
-                                depth=0,
-                                freq=0,
-                                mutated=TRUE,
-                                aggrfreq=0) )
+                                depth=0) )
   }
 
-
-  # Collapse all insertion and deletions respectively, and allow for unexpected categories
-  deletions <- grepl('^del', posdata$type)
-  insertions <- grepl('^ins', posdata$type)
-  other <- grepl('^[^ACGT(ins)(del)(ref)]', posdata$type)
-  posdata$type[deletions] <- 'indel'
-  posdata$type[insertions] <- 'indel'
-  posdata$type[other] <- 'other'
+  # Collapse and rename all insertion and deletions respectively.
+  posdata[grepl('^[ACGT]$', type), type := 'ref']
+  posdata[grepl('^+&', type), type := 'ins']
+  posdata[grepl('^-$', type), type := 'del']
+  
+  # Fix order of categories.
+  posdata[, type := ordered(type, levels=c("ref", "A>C", "A>G", "A>T", "C>A", "C>G", "C>T", "G>A", "G>C", "G>T", "T>A", "T>C", "T>G", "ins", "del"))]
+  
+  # Calculate frequencies and aggregate frequencies by position
+  posdata[, freq := count / depth]
+  posdata[, mutated := freq != 0 & type != 'ref']
+  posdata[(mutated), aggrfreq := sum(freq), by=c('seq', 'pos')]
+  posdata[is.na(aggrfreq), aggrfreq := 0]
 
   # Plot number of positions per mutation type
   print(posdata %>%
-    filter(mutated & type != 'ref_indel') %>%
+    filter(mutated) %>%
     group_by(seq, type) %>%
     summarise(frequency = sum(count>0)) %>%
     ggplot(aes(x = type, y = frequency, fill=type)) +
@@ -105,32 +101,42 @@ for (sf in statsfiles){
       geom_bar(stat = 'identity', width = 0.9) +
       ylab('Number of positions') +
       ggtitle("Number of positions per mutation type.", subtitle=basename(sf)) +
-      guides(fill="none")
+      guides(fill="none") +
+      theme(panel.background = element_rect(fill="white"), 
+            panel.grid.major.y = element_line(colour="grey95"), 
+            panel.grid.minor.y = element_line(colour="grey95"))
   )
 
   # Plot number of reads per mutation type
   print(posdata %>%
-    filter(mutated & type != 'ref_indel') %>%
+    filter(mutated) %>%
     group_by(seq, type) %>%
     summarise(frequency = sum(count)) %>%
     ggplot(aes(x = type, y = frequency, fill=type)) +
       facet_wrap( . ~ seq) +
       geom_bar(stat = 'identity', width=0.9) +
+      #scale_y_log10() +
       ylab('Number of reads') +
       ggtitle("Number of reads showing each mutation type.", subtitle=basename(sf)) +
-      guides(fill="none")
+      guides(fill="none") +
+      theme(panel.background = element_rect(fill="white"), 
+            panel.grid.major.y = element_line(colour="grey95"), 
+            panel.grid.minor.y = element_line(colour="grey95"))
   )
 
   # Pileup plot axis limits
   maxdepth <- max(posdata$depth)
   minpos <- min(posdata$pos)
   maxpos <- max(posdata$pos)
-  maxfreq <- max(posdata %>% filter(type != 'ref_indel') %>% select(aggrfreq))
+  maxfreq <- max(posdata %>% filter(type != 'ref') %>% select(aggrfreq))
   if(ymax != 'NULL')
     maxfreq <- as.numeric(ymax)
-
+  # Automatically determine an additional zoomed level, to get around a few extremely talk peaks that squash everything else.
+  zoomfreq <- quantile(posdata$aggrfreq, probs=c(0.99))
+  
+  
   # Plot mutations pileup
-  print(posdata %>%
+  pp <- posdata %>%
     filter(mutated) %>%
     select(seq, pos, aggrfreq, depth) %>%
     unique() %>%
@@ -141,21 +147,31 @@ for (sf in statsfiles){
       geom_hline(aes(yintercept=quantile(aggrfreq, 0.75)), linetype='dashed', alpha=0.4) +
       geom_line(aes(y=depth / maxdepth * maxfreq * 1.1), colour='grey80') +
       geom_bar(aes(y = aggrfreq, fill=aggrfreq), stat = 'identity') +
-      geom_label_repel(data=posdata %>%
-                       filter(mutated) %>%
-                       select(pos, aggrfreq) %>%
-                       unique() %>%
-                       filter(aggrfreq >= quantile(posdata$aggrfreq, 0.95)),
-                     aes(y=aggrfreq, label=pos), min.segment.length = 0, label.size = 0, fill="transparent", force=1) +
       scale_y_continuous('Frequency (bars)', sec.axis = sec_axis(trans= ~ . * maxdepth / maxfreq / 1.2, name = "Coverage (line)")) +
       scale_fill_gradient(low='blue', high='magenta') +
-      scale_x_continuous('Position', limits = c(minpos, maxpos)) +
+      scale_x_continuous('Position', limits = c(minpos, maxpos), expand = c(0, 0)) +
       # coord_cartesian(ylim=c(NULL, 1.1 * maxfreq)) +
       ggtitle("Fraction of reads with mutation at each position.", subtitle=basename(sf)) +
       theme(legend.position = 'none',
             panel.background = element_rect(fill="white"),
             panel.grid.major.y = element_line(colour='grey95'),
             panel.grid.minor.y = element_line(colour='grey95'))
+  print(pp + geom_label_repel(data=posdata %>%
+                                filter(mutated) %>%
+                                select(pos, aggrfreq) %>%
+                                unique() %>%
+                                filter(aggrfreq >= quantile(posdata$aggrfreq, 0.95)),
+                              aes(y=aggrfreq, label=pos), 
+                              force =1, min.segment.length = 0, label.size = 0, fill="transparent", xlim=c(minpos, maxpos))
+  )
+  print(pp + coord_cartesian(ylim=c(0, zoomfreq)) +
+          geom_label_repel(data=posdata %>%
+                             filter(mutated) %>%
+                             select(pos, aggrfreq) %>%
+                             unique() %>%
+                             filter(aggrfreq >= quantile(posdata$aggrfreq, 0.95)),
+                           aes(y=aggrfreq, label=pos), 
+                           force = 1, min.segment.length = 0, label.size = 0, fill="transparent", xlim=c(minpos, maxpos), ylim=c(0, zoomfreq))
   )
 
   if(collectiveonly != 'yes'){
--- mutPE_workflow.sh
+++ mutPE_workflow.sh
@@ -37,9 +37,7 @@ done
 
 
 module load pandoc/2.0.5
-module load flash/1.2.11-foss-2017a
-module load samtools/1.9-foss-2017a
-
+#module load samtools/1.9-foss-2017a
 
 if [[ ! -d "${base}/${process}/${run}" ]]; then
 	mkdir -p "${base}/${process}/${run}"
@@ -65,25 +63,40 @@ echo "Extracting ${base}/${data}/${run}/*.bam to ${base}/${data}/${run}/*_1/2.fa
 fileutilities.py T ${base}/${data}/${run} --dir .bam | fileutilities.py P --loop srun samtools fastq ,-c 1 ,-1 {dir}/{ali}_1.fastq.gz ,-2 {dir}/{ali}_2.fastq.gz {abs}
 
 echo "Merging pairs in ${base}/${data}/${run} to ${base}/${process}/${run}/*_x25.fastq.gz"
-fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop srun flash ,-t 1 ,-M 150 ,-x 0.25 ,-d ${base}/${process}/${run} ,-o {ali}_x25 ,-z ${base}/${data}/${run}/{ali}_1.fastq.gz ${base}/${data}/${run}/{ali}_2.fastq.gz
+mkdir -p ${base}/${process}/${run}/flash
+module load flash/1.2.11-foss-2017a
+fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop srun flash ,-t 1 ,-M 150 ,-x 0.25 ,-d ${base}/${process}/${run} ,-o {ali}_x25 ,-z ${base}/${data}/${run}/{ali}_1.fastq.gz ${base}/${data}/${run}/{ali}_2.fastq.gz 2\>\&1 \| tee ${base}/${process}/${run}/flash/{ali}.log
+module unload flash
+
+echo "Running FastQC"
+mkdir -p ${base}/${process}/${run}/fastqc
+module load fastqc/0.11.5-java-1.8.0_121
+srun fastqc -o ${base}/${process}/${run}/fastqc ${base}/${process}/${run}/*.extendedFrags.fastq.gz
+module unload fastqc
 
 echo "Aligning ${base}/${data}/${run}/*.fastq.gz to ${base}/${aux}/${bowtie2idx} and outputting to ${base}/${process}/${run}/*_x25.extendedFrags.bam"
-fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop srun ,-c 4 ~/utility_scripts/bowtie2aln.sh ${base}/${aux}/${bowtie2idx} ${base}/${process}/${run} ${base}/${process}/${run} {ali}_x25.extendedFrags false 4 ,--very-sensitive-local
+mkdir -p ${base}/${process}/${run}/bowtie2
+fileutilities.py T ${base}/${data}/${run} --dir _1.fastq.gz | perl -e 'while(<>){~s/_1\.fastq$//;print}' | fileutilities.py P --loop srun ,-c 4 ~/utility_scripts/bowtie2aln.sh ${base}/${aux}/${bowtie2idx} ${base}/${process}/${run} ${base}/${process}/${run} {ali}_x25.extendedFrags false 4 ,--very-sensitive-local 2\> ${base}/${process}/${run}/bowtie2/{ali}.log
 
+echo "Collecting MultiQC"
+mkdir -p ${base}/${process}/${run}
+module load multiqc/1.3-foss-2017a-python-2.7.13
+srun multiqc -f -o ${base}/${process}/${run}/multiqc ${base}/${process}/${run} ${base}/${process}/${run}/flash ${base}/${process}/${run}/fastqc ${base}/${process}/${run}/bowtie2
+module unload multiqc
 
 echo "Stratifying ${base}/${process}/${run}/*.sorted.bam by number of mismatches"
 fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 1 2
 fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 3 10
 fileutilities.py T ${base}/${process}/${run} --dir 'sorted.bam$' | fileutilities.py P --loop srun ~/utility_scripts/split_BAM_by_numMismatches.sh {abs} 11 30
 
-
 echo "Piling up ${base}/${process}/${run}/\d+-\d+.bam onto ${base}/${aux}/${bowtie2idx}.fa"
 fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.bam$' | fileutilities.py P --loop srun samtools mpileup ,-A ,-B ,-f ${base}/${aux}/${bowtie2idx}.${fastasuffix} ,-d 1000000000 {abs} \> {dir}/{bas}.pileup
 
 echo "Counting."
-fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.pileup$' | fileutilities.py P --loop python3 ./analysis/MutPE_quantification.py ,-p {abs} \> {dir}/{bas}.stats
+fileutilities.py T ${base}/${process}/${run} --dir '\d+-\d+.pileup$' | fileutilities.py P --loop summarize_mpileup.py {abs} \> {dir}/{bas}.stats
 
 echo "Visualising ${base}/${process}/${run}/*.stats into ${base}/${results}/${run}/${run//\//_}*.html/pdf"
 mutPE_mutation-stats_viz.R ${base}/${results}/${run} ${run/\//_} NULL no $offsets ${base}/${process}/${run}/*.stats
 
 echo "Finished."
+#module unload samtools
