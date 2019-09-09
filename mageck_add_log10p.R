f0d2e79d52efe217be619680428795a7b38aad62
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-09-09 18:08:33 +0200

    rename

--- mageck_add_log10p.R
+++ mageck_add_log10p.R
@@ -26,17 +26,20 @@ df <- fread(opt$infile, sep="\t", header=TRUE)
 
 
 # Identify and isolate id and pvalue-related columns.
-sel <- (grepl(opt$ppat, names(df), perl=TRUE) |
-          grepl(paste0("^",opt$id,"$"), names(df), perl=TRUE))
+selp <- grepl(opt$ppat, names(df), perl=TRUE)
+selid <- grepl(paste0("^",opt$id,"$"), names(df), perl=TRUE)
+sel <- selp | selid
 subdf <- df[, sel, with=FALSE]
 
 # Transform
-df2 <- data.table(id = subdf[[opt$id]])
-for (n in names(subdf[, -c(opt$id), with=FALSE])) {
-  df2[[n]] = -log10(as.numeric(subdf[[n]]))
-}
+df2 <- as.data.table(lapply(names(subdf[, -c(opt$id), with=FALSE]), function(n) {
+  return( -log10(as.numeric(subdf[[n]])) )
+}))
+df2[is.na(df2)] <- 0
 # Rename
-names(df2) <- gsub(opt$ppat, ".-Log10p.", names(df2), perl=TRUE)
+names(df2) <- gsub(opt$ppat, ".-Log10p.", names(df)[selp], perl=TRUE)
+# Add id for merging
+df2[, id := subdf[[opt$id]]]
 
 fwrite(merge(df, df2, by.x=opt$id, by.y="id", all.x=TRUE),
 			file=opt$outfile, sep="\t", col.names=TRUE, row.names=FALSE)
