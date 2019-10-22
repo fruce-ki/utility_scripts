fec592cd200162c8a23b2b5f13eeeb77bf8ba0e8
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-10-22 17:53:03 +0200

    Update biomarting_example.R

--- biomarting_example.R
+++ biomarting_example.R
@@ -1,24 +1,26 @@
 library(biomaRt)
 library(data.table)
 
-# DT1 <- fread('/Volumes/groups/zuber/zubarchive/USERS/Kimon/sarah/philip2017/results/condition_all.tsv')
-# length(DT1$row_ID) == length(unique(DT1$row_ID))
-# setkey(DT1, row_ID)
-# 
-# DT2 <- fread('/Volumes/groups/zuber/zubarchive/USERS/Kimon/sarah/philip2017/process/count/counts_out_all.txt')
-# length(DT2$row_ID) == length(unique(DT2$row_ID))
-# setkey(DT2, row_ID)
+DT1 <- fread('/Volumes/groups/zuber/zubarchive/USERS/Kimon/markus/M9179_quantseq/process/counts/all_counts.tsv')
+length(DT1$Geneid) == length(unique(DT1$Geneid))
+setkey(DT1, Geneid)
 
 listMarts()
-ensmm = useMart("ENSEMBL_MART_MOUSE")
-listDatasets(ensmm)
-ensmm = useDataset("mc57bl6nj_gene_ensembl", mart=ensmm)
-attributes = listAttributes(ensmm)
+# ens = useMart("ENSEMBL_MART_MOUSE")
+ens = useMart("ENSEMBL_MART_ENSEMBL")
+
+listDatasets(ens)
+# ens = useDataset("mc57bl6nj_gene_ensembl", mart=ens)
+# ens = useDataset("mmusculus_gene_ensembl", mart=ens)
+
+ens = useDataset("hsapiens_gene_ensembl", mart=ens)
+attributes = listAttributes(ens)
 attributes[grepl('[Ee]ntrez', attributes$name, perl=TRUE),]
 
-wanted = c("external_gene_name", "entrezgene")
+wanted = c("ensembl_gene_id", "external_gene_name", "entrezgene_id")
+xref <- getBM(attributes=wanted, mart = ens)
 
-xref <- getBM(attributes=wanted, mart = ensmm)
-fwrite(xref, file="/Volumes/groups/zuber/zubarchive/USERS/Kimon/sarah/philip2017/aux/xref_entrez.txt")
+# fwrite(xref, file="/Volumes/groups/zuber/zubarchive/USERS/Kimon/sarah/philip2017/aux/xref_entrez.txt")
+fwrite(xref, file="/Volumes/groups/zuber/zubarchive/USERS/Kimon/markus/M9179_quantseq/aux/xref_hs_genes.txt", sep="\t", col.names = TRUE)
 
 
