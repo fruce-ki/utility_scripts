7842f66bc46be55f4ec0f477ae2ed1fe84127490
Kimon Froussios <jack_ohara_097@hotmail.com>
2019-10-31 11:37:41 +0100

    bugfixes
    Force inclusion of column header for rownames, to prevent header shifts when merged with other files by fileutilities.
    Wrap reduced formula with as.formula().
    Also changed minCount filter to be independent of number of samples as it can be too aggressive when lots of samples are used.

--- deseq2_runMF.R
+++ deseq2_runMF.R
@@ -4,17 +4,19 @@ library(getopt)
 library(DESeq2)
 
 spec = matrix(c(
-  'baseDir'      , 'b', 2, "character", "Base directory for everything (.)",
+  'baseDir'      , 'b', 1, "character", "Base directory for everything (.)",
   'countsFile'   , 'f', 1, "character", "Tab-separated table of counts with `row_ID` and all the samples",
   'help'         , 'h', 0, "logical",   "Help",
-  'resultsDir'   , 'o', 2, "character", "Directory in which to save the contrast results. If omitted, only the RDS will be output.",
-  'RDSoutdir'    , 'r', 2, "character", "Directory in which to save the raw DESeq2 object (./process)",
+  'resultsDir'   , 'o', 1, "character", "Directory in which to save the contrast results. If omitted, only the RDS will be output.",
+  'RDSoutdir'    , 'r', 1, "character", "Directory in which to save the raw DESeq2 object (./process)",
   'samplesFile'  , 's', 1, "character", "Tab-separated table with `sample` column followed by the variable columns",
   'control'      , 'x', 1, "character", "Comma separated value for each variable to use as reference for all comparisons, in the order variables are listed in samplesfile",
-  'designFormula', 'd', 1, "character", "Design formula"
+  'designFormula', 'd', 1, "character", "Design formula",
+  'reducedFormula','R', 1, "character", "Reduced formula for LR test",
+  'minCount',      'M', 1, "character", "Minimum number of reads across all samples combined for a gene to be considered (30)."
 ), byrow=TRUE, ncol=5)
 opt = getopt(spec)
-#opt <- list(baseDir='/Volumes/groups/obenauf/Kimon_Froussios/chris/Y17_quantseq', countsFile='process/counts/all_counts.tsv', resultsDir='results', RDSoutdir='./process/DE', samplesFile='./description/treatment+history.txt', control='untreated,naive', designFormula='~ treatment + history')
+#opt <- list(baseDir='/Volumes/groups/zuber/zubarchive/USERS/Kimon/markus/M9179_quantseq', countsFile='process/counts/all_counts_named.tsv', resultsDir='./process/DE', RDSoutdir='./process/DE', samplesFile='./aux/CMcontrols.txt', control='post', designFormula='~ time', reducedFormula='~1')
 
 if ( !is.null(opt$help) ) {
   cat(getopt(spec, usage=TRUE))
@@ -26,6 +28,10 @@ if ( is.null(opt$baseDir    ) ) {
 }
 if ( is.null(opt$RDSoutdir ) ) { 
   opt$RDSoutdir = './process' 
+}
+
+if ( is.null(opt$minCount ) ) { 
+  opt$minCount = 30L 
 }
 
 dir.create(file.path(opt$baseDir, opt$RDSoutdir))
@@ -49,9 +55,10 @@ coldata <- read.csv(file.path(opt$baseDir, opt$samplesFile), sep="\t", header=TR
 cdn <- rownames(coldata)
 ctsn <- colnames(cts)
 if (!all(cdn %in% ctsn) ) {
-  if( all(paste0('X',cdn) %in% ctsn) ) {
+  if( all(paste0('X',cdn) %in% ctsn) ) {  # Handle automatic X prepended to names starting with number
     cdn <- paste0('X',cdn)
   } else {
+    print(cdn[!cdn %in% ctsn])
     stop("Error! Samples not in data!")
   }
 }
@@ -72,12 +79,18 @@ for (n in vars){
 
 autoname <- gsub(' |~', '', opt$designFormula)
 
-# Prefilter
-keep <- rowSums(counts(dds)) >= ncol(dds)
+# Prefilter out genes with very poor coverage.
+# This is a speed and memory improvement. DESeq2 later internally applies stricter thresholds.
+keep <- rowSums(counts(dds)) >= opt$minCount
 dds <- dds[keep,]
 
 # DE
-dds <- DESeq(dds)
+if (!is.null(opt$reducedFormula)){
+  dds <- DESeq(dds, test='LRT', reduced = as.formula(opt$reducedFormula))  # LR test
+} else {
+  dds <- DESeq(dds) # Wald test
+}
+
 saveRDS(dds, file=file.path(opt$baseDir, opt$RDSoutdir, paste0(autoname, '_deseq2.RDS')))
 
 # Plain results
@@ -90,8 +103,10 @@ if( !is.null(opt$resultsDir) ) {
   coefficients <- resultsNames(dds)
   for (name in coefficients[2:length(coefficients)]) {
     res <- lfcShrink(dds, coef=name, type='apeglm')       # apeglm is the type recommended by DESeq2
+    
                                                           # Significance and counts are kept the same, but lfc is shrunk.
-    write.table(res, file=file.path(opt$baseDir, opt$resultsDir, paste0(autoname, '_', name, '.tsv')),
-              sep="\t", quote=FALSE, row.names=TRUE, col.names=TRUE)
+    res$mlog10p <- -log10(res$padj)
+    write.table(data.frame(row_ID=rownames(res), res), file=file.path(opt$baseDir, opt$resultsDir, paste0(autoname, '_', name, '.tsv')),
+              sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
   }
 }
