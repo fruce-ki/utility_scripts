27a4ddbbfb676b24ab3a2cac0203a802ae0538b1
Kimon Froussios <jack_ohara_097@hotmail.com>
2020-06-05 11:14:45 +0200

    automate

--- deseq2_runMF.R
+++ deseq2_runMF.R
@@ -3,6 +3,7 @@
 library(getopt)
 library(DESeq2)
 
+
 spec = matrix(c(
   'baseDir'      , 'b', 1, "character", "Base directory for everything (.)",
   'countsFile'   , 'f', 1, "character", "Tab-separated table of counts with `row_ID` and all the samples",
@@ -13,103 +14,234 @@ spec = matrix(c(
   'control'      , 'x', 1, "character", "Comma separated value for each variable to use as reference for all comparisons, in the order variables are listed in samplesfile",
   'designFormula', 'd', 1, "character", "Design formula",
   'reducedFormula','R', 1, "character", "Reduced formula for LR test",
-  'minCount'     , 'M', 1, "character", "Minimum number of reads across all samples combined for a gene to be considered (30)."
+  'forvar',        'F', 1, "character", "Looping variable (ie. carry out the design formula for each level of this var, separately).",
+  'selvar',        'S', 1, "character", "Selection variable (always together with sellev). It is applied BEFORE forvar.",
+  'sellev',        'L', 1, "character", "Selection variable level. Together with selvar, it is used to reduce the samplesFile to just the rows having that value in that variable.",
+  'minCount'     , 'M', 1, "numeric",   "Minimum number of reads across all samples combined for a gene to be considered (10).",
+  'reportTemplate','T', 1, "character", "Template Rmd file for DE report.",
+  'pcutoff',       'p', 1, "numeric",   "P-value cutoff (0.05)",
+  'ntop',          'n', 1, "numeric",   "Number of hits to highlight (50)",
+  'lfcthreshold',  'c', 1, "numeric",   "Log2 fold-change threshold (1)",
+  'nidcols',       'I', 1, "numeric",   "Number of ID columns at the start of the table (1). see also -i",
+  'idcol',         'i', 1, "numeric",   "ID column to use (1). The others will be removed. See also -I.",
+  'bmF',           'B', 0, "logical",   "Disable baseMean filtering. yes/no. (False)",
+  'all',           'A', 0, "logical",   "Enable round-robin-style pairwise comparisons. Otherwise only comparisons only against reference. (False)" 
 ), byrow=TRUE, ncol=5)
+
 opt = getopt(spec)
-#opt <- list(baseDir='/Volumes/groups/zuber/zubarchive/USERS/Kimon/markus/M9179_quantseq', countsFile='process/counts/all_counts_named.tsv', resultsDir='./process/DE', RDSoutdir='./process/DE', samplesFile='./aux/CMcontrols.txt', control='post', designFormula='~ time', reducedFormula='~1')
+# opt <- list(baseDir='/groups/zuber/zubarchive/USERS/Kimon/markus/OTI_slamseq', countsFile='process/quant/counts/all_counts_xref.txt', resultsDir='results/quant/DE', RDSoutdir='process/quant/DE', samplesFile='description/covars.txt', control='control,noch,pre', designFormula='~ gene', forvar='time', selvar='chase', sellev='noch', minCount=10, lfcthreshold=1, nidcols=3, idcol=2, ntop=50, bmF=TRUE, pcutoff=0.05, all=FALSE)
 
 if ( !is.null(opt$help) ) {
   cat(getopt(spec, usage=TRUE))
   q(status=1)
 }
 
+if ( is.null(opt$reportFile) ) {
+  opt$reportFile <- '~/utility_scripts/deseq2_report_template.Rmd'
+}
+
 if ( is.null(opt$baseDir    ) ) { 
-  opt$baseDir    = '.'         
+  opt$baseDir <- '.'         
 }
 if ( is.null(opt$RDSoutdir ) ) { 
-  opt$RDSoutdir = './process' 
+  opt$RDSoutdir <- './process' 
 }
 
-if ( is.null(opt$minCount ) ) { 
-  opt$minCount = 30L 
+if ( ! is.null(opt$minCount ) ) { 
+  opt$minCount <- 10
+}
+if ( is.null(opt$ntop ) ) { 
+  opt$ntop <- 50
 }
 
-dir.create(file.path(opt$baseDir, opt$RDSoutdir))
+if ( is.null(opt$pcutoff ) ) { 
+  opt$pcutoff <- 0.05
+}
+if ( is.null(opt$lfcthreshold ) ) { 
+  opt$lfcthreshold <- 1
+}
+
+if ( is.null(opt$nidcols ) ) { 
+  opt$nidcols <- 1
+}
+if ( is.null(opt$idcol ) ) { 
+  opt$idcol <- 1
+}
+
+if ( is.null(opt$bmF ) ) { 
+  opt$bmF <- FALSE
+}
+if ( is.null(opt$all ) ) { 
+  opt$all <- FALSE
+}
+opt$bmF <- !opt$bmF    # Convert -B flag functionality from switch-on to switch-off. 
+
+
+
+# Output destinations
+dir.create(file.path(opt$baseDir, opt$RDSoutdir), recursive=TRUE)
 
 if ( !is.null(opt$resultsDir ) ) { 
-  dir.create(file.path(opt$baseDir, opt$resultsDir))
+  dir.create(file.path(opt$baseDir, opt$resultsDir), recursive=TRUE)
 }
+
 
 # Input
-cts <- round(as.matrix(read.csv(file.path(opt$baseDir, opt$countsFile), sep="\t", header=TRUE, row.names=1)), digits=0)
-coldata <- read.csv(file.path(opt$baseDir, opt$samplesFile), sep="\t", header=TRUE, row.names='sample')
+covars <- as.data.frame(read.csv(file.path(opt$baseDir, opt$samplesFile), sep="\t", header=TRUE, row.names='sample'))
+cts <- read.csv(file.path(opt$baseDir, opt$countsFile), sep="\t", header=TRUE)
 
-# Remove covariates with a single category (unnecessary since the design is provided explicitly).
-# for (n in names(coldata)){
-#   if ( length(unique(coldata[,n])) < 2 ) {
-#     message(paste0("Dropped `", n, "` from the covariates table due to uniform value."))
-#     coldata[[n]] <- NULL
-#   }
-# }
+# Cut out extra annotation/id columns
+xref <- cts[, 1:opt$nidcols]
+cts <- round(as.matrix(cts[, (opt$nidcols+1):ncol(cts)]), digits=0)
+rownames(cts) <- xref[, opt$idcol]
 
-# Remove unused samples, and ensure the rest are in the correct order (simplifies subset comparisons from larger datasets).
-cdn <- rownames(coldata)
-ctsn <- colnames(cts)
-if (!all(cdn %in% ctsn) ) {
-  if( all(paste0('X',cdn) %in% ctsn) ) {  # Handle automatic X prepended to names starting with number
-    cdn <- paste0('X',cdn)
-  } else {
-    print(cdn[!cdn %in% ctsn])
-    stop("Error! Samples not in data!")
-  }
-}
-cts <- cts[, match(cdn, ctsn)]
+# Calculate RPMs. 
+# (for 3'seq like quantseq and slamseq, length normalisation is not approriate)
+colsums <- colSums(cts, na.rm=TRUE)
+RPMs <- as.matrix(as.data.frame(lapply(colnames(cts), function(n) { cts[,n] / colsums[n] * 1e6 }) ))
+colnames(RPMs) <- colnames(cts)
+rownames(RPMs) <- rownames(cts)
 
-# Setup
-vars = names(coldata)[ vapply(names(coldata), function(x) { grepl(x, opt$designFormula) }, logical(1)) ] # only variables used in the design
+# Identify control level for the variables (it's less command editing to always provide all the controls, even the ones for variables that will be dropped).
 refs <- strsplit(opt$control, ',')[[1]]
-names(refs) <- vars
+names(refs) <- names(covars)
 
-dds <- DESeqDataSetFromMatrix(countData = cts,
-                              colData = coldata,
-                              design = as.formula(opt$designFormula))
-# set reference condition(s)
-for (n in vars){
-  dds[[n]] <- relevel(dds[[n]], ref=refs[n])
+# Apply sample selection
+if ( !is.null(opt$selvar) ) {
+  stopifnot(!is.null(opt$sellev) & opt$sellev %in% covars[[opt$selvar]])
+  
+  # Select the samples and then drop the variable, since it has only one value left after selection.
+  covars <- covars[covars[[opt$selvar]] == opt$sellev, ]
+  covars[opt$selvar] <- NULL
 }
 
-autoname <- gsub(' |~', '', opt$designFormula)
 
-# Prefilter out genes with very poor coverage.
-# This is a speed and memory improvement. DESeq2 later internally applies stricter thresholds.
-keep <- rowSums(counts(dds)) >= opt$minCount
-dds <- dds[keep,]
-
-# DE
-if (!is.null(opt$reducedFormula)){
-  dds <- DESeq(dds, test='LRT', reduced = as.formula(opt$reducedFormula))  # LR test
+#################
+# Repeat analysis for every level of the looping variable.
+subcovars <- NULL
+if (!is.null(opt$forvar)) {
+  # in case not all combinatinons of variables exist
+  covars[[opt$forvar]] <- droplevels(covars[[opt$forvar]])
+  
+  subcovars <- lapply(levels(covars[[opt$forvar]]), function(V) {
+    # V <- levels(covars[[opt$forvar]])[1]
+    
+    # Select relevant rows and drop the column
+    coldata <- covars[covars[[opt$forvar]] == V, ]
+    coldata[opt$forvar] <- NULL
+    return(coldata)
+  })
+  names(subcovars) <- levels(covars[[opt$forvar]])
 } else {
-  dds <- DESeq(dds) # Wald test
+  subcovars <- list(covars)
+  names(subcovars) <- 'default'
 }
 
-saveRDS(dds, file=file.path(opt$baseDir, opt$RDSoutdir, paste0(autoname, '_deseq2.RDS')))
 
-coefficients <- resultsNames(dds)
-if( !is.null(opt$resultsDir) ) {
-  # Plain results
-  for (name in coefficients[2:length(coefficients)]) {
-    res <- results(dds, name=name)
-    res$mlog10p <- -log10(res$padj)
-    write.table(data.frame(row_ID=rownames(res), res), file=file.path(opt$baseDir, opt$resultsDir, paste0(autoname, '_', name, '.lfc.tsv')),
-                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
-  }
-  # Shrunk LFC supposedly better for plotting and ranking
-  for (name in coefficients[2:length(coefficients)]) {
-    res <- lfcShrink(dds, coef=name, type='apeglm')       # apeglm is the type recommended by DESeq2
+for (V in names(subcovars)){
+  # V <- names(subcovars)[1]
+  coldata <- subcovars[[V]]
+  
+  # Identify the (remaining) variables.
+  vars <- names(coldata)
+  # Identify the ones relevant to the formula(s), and drop the rest.
+  relevant <- vapply(names(coldata), function(x) { grepl(x, opt$designFormula) }, logical(1))
+  vars <- names(coldata)[relevant]
+  refs <- refs[vars]
+  coldata <- coldata[, c(relevant), drop=FALSE]
+  
+  # Remove unused samples from the counts table, 
+  # and ensure the rest are in the same order as the covariates table.
+  cdn <- rownames(coldata)
+  ctsn <- colnames(cts)
+  subcts <- cts[, match(cdn, ctsn)]
+  subcts[is.na(subcts)] <- 0
+  subrpm <- RPMs[, match(cdn, colnames(RPMs))]
+  subrpm[is.na(subrpm)] <- 0
 
-                                                          # Significance and counts are kept the same, but lfc is shrunk.
-    res$mlog10p <- -log10(res$padj)
-    write.table(data.frame(row_ID=rownames(res), res), file=file.path(opt$baseDir, opt$resultsDir, paste0(autoname, '_', name, '.shrunken.tsv')),
-              sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+  # DESeq2 begins here
+  DDS <- DESeqDataSetFromMatrix(countData = subcts,
+                                colData = coldata,
+                                design = as.formula(opt$designFormula))
+  # Set reference condition(s)
+  for (n in vars){
+    DDS[[n]] <- relevel(DDS[[n]], ref=refs[n])
   }
+  
+  # Prefilter out genes with very poor coverage.
+  # This is a speed and memory improvement. DESeq2 later internally applies additional thresholds.
+  keep <- rowSums(counts(DDS)) >= opt$minCount
+  DDS <- DDS[keep, ]
+  subrpm <- subrpm[keep, ]
+  # subxref <- xref[keep, ]
+  
+  # Prepare file name.
+  autoname <- NULL
+  if (!is.null(opt$selvar)) {
+    autoname <- paste(opt$selvar, opt$sellev, sep="-")
+  }
+  if (!is.null(opt$forvar)) {
+    autoname <- paste(autoname, paste(opt$forvar, V, sep="-"), sep="_")
+  }
+  
+  ddsrds <- file.path(opt$baseDir, opt$RDSoutdir, paste0(paste(autoname, gsub(' |~', '', opt$designFormula), sep='_'), '_deseq2data.RDS'))
+  saveRDS(DDS, file=ddsrds)
+  rpmrds <- file.path(opt$baseDir, opt$RDSoutdir, paste0(paste(autoname, gsub(' |~', '', opt$designFormula), sep='_'), '_rpm.RDS'))
+  saveRDS(subrpm, file=rpmrds)
+  
+  # Fire up an Rmd report
+  rmarkdown::render(opt$reportFile, 
+                    # output_file = paste0(paste(autoname, name, sep='_'), '_deseq2.html'), 
+                    output_file = paste0(paste(autoname, gsub(' |~', '', opt$designFormula), sep='_'), '_deseq2.html'),
+                    output_dir = file.path(opt$baseDir, opt$resultsDir),
+                    params=list(ntop = opt$ntop,
+                                pcutoff = opt$pcutoff,
+                                lfcthreshold = opt$lfcthreshold,
+                                baseDir = opt$baseDir,
+                                resultsDir = opt$resultsDir,
+                                prefix = autoname,
+                                derds = ddsrds,
+                                rmrds = rpmrds,
+                                minCount = opt$minCount,
+                                filterBaseMean = opt$bmF,
+                                reducedFormula = opt$reducedFormula,
+                                roundrobin=opt$all)
+                    )
+  unlink(rpmrds)
+  
+  # # DE
+  # if (!is.null(opt$reducedFormula)){
+  #   DDS <- DESeq(DDS, test='LRT', reduced = as.formula(opt$reducedFormula))  # LR test
+  # } else {
+  #   DDS <- DESeq(DDS) # Wald test
+  # }
+  #
+  # # Compute contrasts with just the basic table outputs, no figures.
+  # coefficients <- resultsNames(DDS)
+  # if( !is.null(opt$resultsDir) ) {
+  #   # Plain results
+  #   for (name in coefficients[2:length(coefficients)]) {
+  #     # name <- coefficients[2]
+  #     
+  #     res <- results(DDS, name=name, alpha=opt$pcutoff)
+  #     res$mlog10p <- -log10(res$padj)
+  #     write.table(data.frame(row_ID=rownames(res), res), file=file.path(opt$baseDir, opt$resultsDir, paste0(autoname, '_', name, '.lfc.tsv')),
+  #                 sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+  #   }
+  #   
+  #   # Shrunk LFC supposedly better for plotting and ranking
+  #   for (name in coefficients[2:length(coefficients)]) {
+  #     res <- lfcShrink(DDS, coef=name, type='apeglm')       # apeglm is the type recommended by DESeq2
+  #     
+  #     # Significance and counts are kept the same, but lfc is shrunk.
+  #     res$mlog10p <- -log10(res$padj)
+  #     write.table(data.frame(row_ID=rownames(res), res), file=file.path(opt$baseDir, opt$resultsDir, paste0(autoname, '_', name, '.shrunken.tsv')),
+  #                 sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+  #   }
+  # }
+  
 }
+
+
+
+
---
+++ pca_run.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+library(getopt)
+
+spec = matrix(c(
+  'help'         , 'h', 0, "logical",   "Help",
+  'countsFile'   , 'f', 1, "character", "Tab-separated table of counts with `row_ID` and all the samples",
+  'samplesFile'  , 's', 1, "character", "Tab-separated table with `sample` column followed by the variable columns",
+  'nidcols',       'I', 1, "numeric",   "Number of ID columns at the start of the table (1). see also -i",
+  'idcol',         'i', 1, "numeric",   "ID column to use (1). The others will be removed. See also -I.",
+  'resultsDir'   , 'o', 1, "character", "Directory in which to save the contrast results. If omitted, only the RDS will be output.",
+  'reportTemplate','T', 1, "character", "Template Rmd file for DE report."
+), byrow=TRUE, ncol=5)
+
+opt = getopt(spec)
+# opt <- list(countsFile='/groups/zuber/zubarchive/USERS/Kimon/markus/OTI_slamseq_deep/process/quant/all_counts_rpm_xref.txt', samplesFile='/groups/zuber/zubarchive/USERS/Kimon/markus/OTI_slamseq_deep/description/covars.txt', resultsDir='/groups/zuber/zubarchive/USERS/Kimon/markus/OTI_slamseq_deep/results/quant/PCA', nidcols=3, idcol=2)
+
+if ( !is.null(opt$help) ) {
+  cat(getopt(spec, usage=TRUE))
+  q(status=1)
+}
+
+if ( is.null(opt$reportFile) ) {
+  opt$reportFile <- '~/utility_scripts/PCA_report_template.Rmd'
+}
+
+if ( is.null(opt$nidcols ) ) { 
+  opt$nidcols <- 1
+}
+
+if ( is.null(opt$resultsDir    ) ) { 
+  opt$resultsDir <- '.'         
+}
+
+
+dir.create(opt$resultsDir, recursive=TRUE)
+
+# Fire up the Rmd report
+rmarkdown::render(opt$reportFile, 
+                  output_file = sub('.txt|.tsv', '_pca.html', basename(opt$countsFile)),
+                  output_dir = opt$resultsDir,
+                  params=list(cts = opt$countsFile,
+                              covars = opt$samplesFile,
+                              nidcols = opt$nidcols,
+                              idcol = opt$idcol,
+                              pdf = file.path(opt$resultsDir, sub('.txt|.tsv', '_pca.pdf', basename(opt$countsFile))),
+                              prefix = file.path(opt$resultsDir, sub('.txt|.tsv', '', basename(opt$countsFile))) )
+)
