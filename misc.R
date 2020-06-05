03ac18fb436ffac675d82d5b6f9c83cd6544d80c
Kimon Froussios <jack_ohara_097@hotmail.com>
2020-06-05 11:16:11 +0200

    Update misc.R

--- misc.R
+++ misc.R
@@ -1,18 +1,81 @@
-### PCA
-########
-
 library(data.table)
 library(matrixStats)
 library(ggplot2)
 library(ggExtra)
 library(plotly)
 library(htmltools)
-# Calculate PCA and plots
-do_pca <- function(counts, covars, center=TRUE, scale=TRUE, loadthresh = 0.75){
-  # Convert table to matrix
-  countsmat <- as.matrix(counts[, 2:length(counts)])
-  rownames(countsmat) <- counts$Geneid
+
+
+
+### Correlations within the dataframe.
+######################################
+# colnames and rownames yes, non-numeric columns no.
+my_pairwise_internal_corels <- function(mat, method = "pearson") {
+  # Correlations
+  cormat <- cor(mat, method=method)
   
+  # Cluster
+  hcfit <- hclust(dist(scale(cormat, center=TRUE)))
+  cormat <- cormat[hcfit$order, hcfit$order]
+  rn <- rownames(cormat)
+  
+  cormat2 <- cormat # Duplicate, for colour fills.
+  # Delete diagonal half for the numeric labels.
+  for (r in 1:nrow(cormat)) {
+    for (c in 1:ncol(cormat)) {
+      if (c <= r) {
+        cormat[r, c] <- NA_real_
+      }
+    }
+  }
+  
+  # Restructure for plotting.
+  cormat <- t(cormat)
+  cormat2 <- t(cormat2)
+  cormat <- as.data.table(t(cormat))
+  cormat2 <- as.data.table(t(cormat2))
+  cormat[, observation1 := factor(rn, ordered=TRUE, levels=rn)]
+  cormat2[, observation1 := factor(rn, ordered=TRUE, levels=rn)]
+  cormat <- melt(cormat, id.vars = "observation1", value.name = "Correlation", variable.name = "observation2")
+  cormat2 <- melt(cormat2, id.vars = "observation1", value.name = "Correlation", variable.name = "observation2")
+  cormat[, observation2 := factor(observation2, ordered=TRUE, levels=rev(rn))]
+  cormat2[, observation2 := factor(observation2, ordered=TRUE, levels=rev(rn))]
+  
+  # Relative to full range of values -1,1 (better context)
+  p1 <- ggplot(cormat2, aes(x=observation1, y=observation2)) +
+    geom_tile(aes(fill=Correlation)) +
+    geom_text(data=cormat, aes(label=round(Correlation, 2), colour=Correlation >= -0.70 & Correlation <= 0.70 )) +
+    scale_fill_gradientn(limits=c(-1, 1), colors=c("lightskyblue", "blue", "darkblue", "black", "darkred", "red", "gold"), na.value = "transparent" ) +
+    scale_colour_manual(values=c("black", "white"), na.value="transparent", guide="none") +
+    labs(title=paste(paste(toupper(substr(method, 1, 1)), tolower(substr(method, 2, nchar(method))), "'s", sep=""), "correlation"), 
+         x='', y='') +
+    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5),
+          panel.grid.major = element_blank() )
+  
+  # Relative to actual value range min,max (better contrast)
+  m <- min(cormat2$Correlation, na.rm=TRUE)
+  M <- max(cormat2$Correlation, na.rm=TRUE)
+  colourswitch <- c( m + 0.16 * (M-m),  m + 0.85 * (M-m) ) # switch letter colour at 15% and 85% of the value range, to stay legible over the fill gradient
+  # Quantiles not suitable for this, because the gradient is not affected by values inbetween max and min.
+  p2 <- ggplot(cormat2, aes(x=observation1, y=observation2)) +
+    geom_tile(aes(fill=Correlation)) +
+    geom_text(data=cormat, aes(label=round(Correlation, 2), colour=( Correlation >= colourswitch[1] & Correlation <= colourswitch[2] ))) +
+    scale_fill_gradientn(colors=c("lightskyblue", "blue", "darkblue", "black", "darkred", "red", "gold"), na.value = "transparent" ) +
+    scale_colour_manual(values=c("black", "white"), na.value="transparent", guide="none") +
+    labs(title=paste(paste(toupper(substr(method, 1, 1)), tolower(substr(method, 2, nchar(method))), "'s", sep=""), "correlation"), 
+         x='', y='') +
+    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5),
+          panel.grid.major = element_blank() )
+  
+  return( list(full=p1, dyna=p2) )
+}
+
+
+
+### PCA and plots
+########
+# covars is DE style colData
+do_pca <- function(countsmat, covars, center = TRUE, scale = TRUE, loadthresh = 0.75){
   # Gene variances
   genevar <- data.table(name = rownames(countsmat),
                         Mean = rowMeans(countsmat),
@@ -23,7 +86,6 @@ do_pca <- function(counts, covars, center=TRUE, scale=TRUE, loadthresh = 0.75){
   
   # Add covariates info
   counts <- cbind(covars, as.data.table(countsmat))
-  rownames(counts) <- rownames(countsmat)
   
   # Calculate principal components (after getting rid of zero-variance genes that can't be standardized).
   
@@ -34,8 +96,9 @@ do_pca <- function(counts, covars, center=TRUE, scale=TRUE, loadthresh = 0.75){
   pca <- prcomp(countsmat, center = center, scale. = scale)
   srn <- sqrt(nrow(countsmat) - 1)
   pc <- sweep(pca$x, 2, 1 / (pca$sdev * srn), FUN = '*')
-  pc <- cbind(pc, data.frame(name = rownames(pc)))
-  pc <- merge(pc, covars, by.x = "name", by.y="sample", all = TRUE)
+  pc <- cbind(pc, data.frame(sample = rownames(pc)))
+  covars <- cbind(covars, data.frame(sample = rownames(covars)))
+  pc <- as.data.frame(merge(pc, covars, by="sample", all = TRUE))
   npc <- sum(pca$sdev > 1)
   
   # Screeplot.
@@ -99,147 +162,44 @@ do_pca <- function(counts, covars, center=TRUE, scale=TRUE, loadthresh = 0.75){
   
   # Correlation of the samples for the selected features only.
   
-  genec <- cor(t(countsmat[, sel]))
-  hcfit <- hclust(dist(t(genec)))
-  rn <- rownames(genec)
-  genec <- as.data.table(genec)
-  genec[, observation1 := rn]
-  genec <- melt(genec, id.vars = "observation1", value.name = "correlation", variable.name = "observation2")
-  genec[, observation1 := factor(observation1, ordered=TRUE, 
-                                 levels=rn[hcfit$order]) ]
-  genec[, observation2 := factor(observation2, ordered=TRUE, 
-                                 levels=rn[hcfit$order]) ]
-  
-  pcor1 <- ggplot(genec, aes(x=observation1, y=observation2, fill=correlation, label=correlation)) +
-    geom_tile() +
-    # geom_text(colour="white") +
-    # scale_x_discrete(position="top") +
-    scale_fill_gradientn(limits=c(-1, 1), colors=c("steelblue2", "blue", "darkblue", "black", "darkred", "red", "orange", "yellow") ) +
-    labs(x='', y='', title="Correlation of samples", subtitle="(top loading in first 3PCs)") +
-    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
-  
-  # # Correlation of the selected features.
-  # 
-  # genec <- cor(countsmat[, sel])
-  # hcfit <- hclust(dist(t(genec)))
-  # rn <- rownames(genec)
-  # genec <- as.data.table(genec)
-  # genec[, observation1 := rn]
-  # genec <- melt(genec, id.vars = "observation1", value.name = "correlation", variable.name = "observation2")
-  # genec[, observation1 := factor(observation1, ordered=TRUE, 
-  #                                levels=rn[hcfit$order]) ]
-  # genec[, observation2 := factor(observation2, ordered=TRUE, 
-  #                                levels=rn[hcfit$order]) ]
-  # 
-  # pcor2 <- ggplot(genec, aes(x=observation1, y=observation2, fill=correlation, label=correlation)) +
-  #   geom_tile() +
-  #   # geom_text(colour="white") +
-  #   # scale_x_discrete(position="top") +
-  #   scale_fill_gradientn(limits=c(-1, 1), colors=c("steelblue2", "blue", "darkblue", "black", "darkred", "red", "orange") ) +
-  #   labs(x='', y='', title="Correlation of top-loading features", subtitle="(in first 3 PCs)") +
-  #   theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
-  # if (sqrt(dim(genec)[1]) >50) {
-  #   pcor2 <- pcor2 + theme(axis.text.x = element_blank(), 
-  #                          axis.text.y = element_blank(),
-  #                          axis.ticks = element_blank() )
-  # }
+  pcor <- my_pairwise_internal_corels(t(countsmat[, sel]))
   
   # Plot first 3 PCs in 2D pairs.
   # Highlight one variable at a time.
   pc12 <- lapply(ig, function(varname) { 
     return( 
-      ggplot(pc, aes_string(x="PC1", y="PC2", label="name", colour=varname)) +
+      ggplot(pc, aes_string(x="PC1", y="PC2", label="sample", colour=varname)) +
         geom_point(shape=16) +
         coord_fixed()
     )})
-  # do.call(grid.arrange, c(l1, ncol=2))
   
   pc13 <- lapply(ig, function(varname) { 
     return( 
-      ggplot(pc, aes_string(x="PC1", y="PC3", label="name", colour=varname)) +
+      ggplot(pc, aes_string(x="PC1", y="PC3", label="sample", colour=varname)) +
         geom_point(shape=16) +
         coord_fixed()
     )})
-  # do.call(grid.arrange, c(l2, ncol=2))
   
-  pc23 <- lapply(ig, function(varname) { 
+  pc32 <- lapply(ig, function(varname) { 
     return( 
-      ggplot(pc, aes_string(x="PC2", y="PC3", label="name", colour=varname)) +
+      ggplot(pc, aes_string(x="PC3", y="PC2", label="sample", colour=varname)) +
         geom_point(shape=16) +
         coord_fixed()
     )})
-  # do.call(grid.arrange, c(l3, ncol=2))
   
   return(list(pca=pca, 
               nvars=nvars, 
               nPC=npc, 
               pimp=pimp, 
               load1=sel1, load2=sel2, load3=sel3,
-              pc_1_2=pc12, pc_1_3=pc13, pc_2_3=pc23,
+              pc_1_2=pc12, pc_1_3=pc13, pc_3_2=pc32,
               pvar1=pvar1, pvar2=pvar2, 
-              pcor1=pcor1 #, pcor2=pcor2
+              pcor1=pcor[[1]] , pcor2=pcor[[2]]
   ))
 }
 
-### Correlations within the dataframe.
-######################################
+  
 
-# library(ggplot2)
-# library(data.table)
-# colnames and rownames yes, non-numeric columns no.
-my_pairwise_internal_correls <- function(mat, method="pearson", triangular=FALSE) {
-  # Correlations
-  cormat <- cor(mat, method=method)
-  
-  # Cluster
-  hcfit <- hclust(dist(scale(cormat, center=TRUE)))
-  cormat <- cormat[hcfit$order, hcfit$order]
-  rn <- rownames(cormat)
-  cormat2 <- cormat # Duplicate,
-  
-  
-  # Delete diagonal half for the numeric labels. But keep the heatmap square, because pretty.
-  for (r in 1:nrow(cormat)) {
-    for (c in 1:ncol(cormat)) {
-      if (c < r) {
-        cormat[r, c] <- NA_real_
-      }
-    }
-  }
-  # Unless I want it to be all triangular
-  if (triangular) {
-    for (r in 1:nrow(cormat2)) {
-      for (c in 1:ncol(cormat2)) {
-        if (c < r) {
-          cormat2[r, c] <- NA_real_
-        }
-      }
-    }
-  }
-  
-  cormat <- as.data.table(t(cormat))
-  cormat[, observation1 := factor(rn, ordered=TRUE, levels=rn)]
-  cormat <- melt(cormat, id.vars = "observation1", value.name = "Correlation", variable.name = "observation2")
-  cormat[, observation2 := factor(observation2, ordered=TRUE, levels=rn)]
-  cormat2 <- as.data.table(t(cormat2))
-  cormat2[, observation1 := factor(rn, ordered=TRUE, levels=rn)]
-  cormat2 <- melt(cormat2, id.vars = "observation1", value.name = "Correlation", variable.name = "observation2")
-  cormat2[, observation2 := factor(observation2, ordered=TRUE, levels=rn)]
-  
-  p <- ggplot(cormat2, aes(x=observation1, y=observation2)) +
-    geom_tile(aes(fill=Correlation)) +
-    geom_text(data=cormat, aes(label=round(Correlation, 2), colour=abs(Correlation)>=0.5), size=2) +
-    scale_fill_gradientn(limits=c(-1, 1), colors=c("lightskyblue", "dodgerblue", "blue", "darkblue", "black", "darkred", "red", "orange", "yellow"), na.value = "transparent" ) +
-    scale_colour_manual(values=c("white", "black"), guide="none") +
-    labs(x='', y='') +
-    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
-  if (!triangular) {
-    p <- p + theme(panel.grid.major = element_blank())
-  }
-  return(p)
-}
-  
-  
 ### GGpairs for continuous variables within a dataframe.
 ########################################################
 
