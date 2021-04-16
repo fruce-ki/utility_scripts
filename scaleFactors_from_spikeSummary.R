2c477b25aeb5523806c484baa4fd4b01ec729d37
fruce-ki <jack_ohara_097@hotmail.com>
2021-04-16 18:59:08 +0200

    invert factor

--- scaleFactors_from_spikeSummary.R
+++ scaleFactors_from_spikeSummary.R
@@ -16,7 +16,7 @@ gm_mean = function(x, na.rm=TRUE){
 target <- gm_mean(DT$spike_counts)
 
 
-DT["spike_factor"] <- target / DT$spike_counts  # Multiplication factor to apply so as to get all spikes to the same value
-# DT["subject_scaled"] <- DT$subject_counts * DT$spike_factor
+DT["spike_factor"] <- target / DT$spike_counts  # Multiplication factor to apply so as to get all spikeins to the same target value.
+DT["subject_factor"] <- 1 / DT["spike_factor"]  # Multiplication factor to apply to the subject library,
 
-write.table(DT[, c("row_ID", "spike_factor")], file=sub('txt|csv|tsv', 'factors.txt', args[1], perl=TRUE), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+write.table(DT[, c("row_ID", "subject_factor")], file=sub('txt|csv|tsv', 'factors.txt', args[1], perl=TRUE), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
