019efd2c68bd04dc208d270c51e5e1c0f23c7c38
fruce-ki <jack_ohara_097@hotmail.com~>
2021-05-31 17:48:42 +0200

    comments

--- chromosome_surgery.py
+++ chromosome_surgery.py
@@ -7,20 +7,18 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 usage = "Chromosome surgery: Splice something into and/or out of a chromosome."
 # Main Parsers
 parser = ArgumentParser(description=usage, formatter_class=RawDescriptionHelpFormatter)
-parser.add_argument("-o", "--output", type=str, required=True, help="Output FASTA.")
 parser.add_argument("-f", "--fasta", type=str, required=True, help="Input FASTA.")
+parser.add_argument("-o", "--output", type=str, required=True, help="Output FASTA.")
 parser.add_argument("-c", "--cid", type=str, required=True, help="Chromosome ID to edit.")
 parser.add_argument("-i", "--insert", type=str, required=False, help="FASTA of sequence to insert.")
 parser.add_argument("-I", "--incision", type=str, required=False, help="1-based nucleotide after which to insert the insert.")
-parser.add_argument("-e", "--excision_start", type=str, required=False, help="1-based nucleotide that is the first to delete.")
-parser.add_argument("-E", "--excision_end", type=str, required=False, help="1-based nucleotide that is the last to delete.")
+parser.add_argument("-e", "--excision_start", type=str, default=0, required=False, help="1-based nucleotide that is the first to delete (0).")
+parser.add_argument("-E", "--excision_end", type=str, default=0, required=False, help="1-based nucleotide that is the last to delete (0).")
 args = parser.parse_args()
 
 
 # Harmless defaults
 splice_in = ''
-excision_start = 0 # left closed
-excision_end = 0   # right open
 
 
 # Get insert
@@ -30,20 +28,21 @@ if args.insert:
         splice_in = record.seq
     
     # No need to shift the incision coordinate. 
-    # The 1-based right-closed index after which to cut (human) is the same as the 0-based right-open substring end before the cut (python)
+    # The 1-based right-closed index after which to cut is the same location as the 0-based right-open substring end before the cut.
 
 if args.excision_start and args.excision_end:
     excision_start = args.excision_start
     excision_end = args.excision_end
     # Pythonize start coordinate from 1-based left-closed to 0-based left-closed.
     excision_start -= 1
-    # No need to change the end coordinate. The 1-based right-closed index is the same as the 0-based right-open substring end.
+    # No need to change the end coordinate. The 1-based right-closed index is the same location as the 0-based right-open substring end.
 
 if args.insert and args.excision_start:
     # Do excision after the incision. 
     # Adjust coordinates.
     if args.excision_start > args.incision and args.excision_end > args.incision:
         excision_start = args.excision_start + len(splice_in)
+        excision_end = args.excision_end + len(splice_in)
    
     elif args.excision_start < args.incision and args.excision_end < args.incision:
         pass    # The incision will be applied first, no need to adjust it. The excision is unaffected by the incision anyway.
