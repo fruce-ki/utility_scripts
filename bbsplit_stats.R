455c7302d3f1ef3f229913f8df56cae32cee63ed
Kimon Froussios <kimon.froussios@neurolentech.com>
2024-01-29 18:40:02 +0100

    simplify plotting, switch to thresholding the counts

--- bbsplit_stats.R
+++ bbsplit_stats.R
@@ -2,19 +2,25 @@
 
 library(data.table)
 library(ggplot2)
+library(patchwork)
 library(getopt)
 
 spec = matrix(c(
   'baseDir',      'b', 1, "character", "Full path to the project dir for nf-core/rnaseq.",
-  'bbDir',        'd', 1, "character", "Relative sub-path to the bbsplit results directory."
+  'bbDir',        'd', 1, "character", "Relative sub-path to the bbsplit results directory.",
+  'pc_thresh',    'p', 1, "numeric", "Minimum acceptable percentage of human content (20).",
+  'cnt_thresh',   'c', 1, "numeric", "Minimum acceptable read count for human content (5000000)."
 ), byrow=TRUE, ncol=5)
 
 opt <- getopt(spec)
 
-opt <- list(baseDir = '/SCRATCH/sBatch1/', bbDir = 'outputs/bbsplit/', sampleSheet = 'samplesheet.rnaseq.csv')
+# opt <- list(baseDir = '/SCRATCH/sBatch1/', bbDir = 'outputs/bbsplit/', sampleSheet = 'samplesheet.rnaseq.csv')
 
 files <- dir(file.path(opt$baseDir, opt$bbDir))
 files <- files[grepl('.stats.txt', files)]
+
+if (is.null(opt$pc_thresh)) opt$pc_thresh <- 20
+if (is.null(opt$cnt_thresh)) opt$cnt_thresh <- 5e6
 
 DT <- Reduce(rbind,
              lapply(files, function(x) {
@@ -30,54 +36,90 @@ DT <- Reduce(rbind,
 
 DT[, row := 1:.N]
 
+
+
+
 DT1 <- DT[, c("row", "sample", "name", "unambiguousReads", "ambiguousReads"), with=FALSE]
 DT2 <- DT[, c("row", "sample", "name", "unambiguousReadsPC", "ambiguousReadsPC"), with=FALSE]
 
-DT1 <- melt(DT1, id.vars = c("row", "sample", "name"), variable.name = "type", value.name = "PC", measure.vars = c("unambiguousReads", "ambiguousReads"))
+DT1 <- melt(DT1, id.vars = c("row", "sample", "name"), variable.name = "type", value.name = "Count", measure.vars = c("unambiguousReads", "ambiguousReads"))
 DT2 <- melt(DT2, id.vars = c("row", "sample", "name"), variable.name = "type", value.name = "PC", measure.vars = c("unambiguousReadsPC", "ambiguousReadsPC"))
 
-p1 <- ggplot(DT1, aes(x=sample, y=PC, fill=name)) +
-  facet_wrap(~ type, nrow = 3, scales = "free_y") +
+# Ambiguous are counted doubly
+DT1 <- DT1[!(name != 'primary' & type == 'ambiguousReads'), ]
+DT2 <- DT2[!(name != 'primary' & type == 'ambiguousReadsPC'), ]
+DT1[type == 'ambiguousReads', name := 'ambiguous']
+DT2[type == 'ambiguousReadsPC', name := 'ambiguous']
+
+DT1[, name := factor(name, ordered = TRUE, levels = rev(c('primary', sort(unique(DT1[name != 'primary', name])))))]
+DT2[, name := factor(name, ordered = TRUE, levels = rev(c('primary', sort(unique(DT2[name != 'primary', name])))))]
+
+
+fills <- c('orange3', 'darkgreen', rep('yellow3', times = length(levels(DT1$name)) - 2))
+names(fills) <- c('ambiguous', 'primary', levels(DT1$name)[1:(length(levels(DT1$name)) - 2)])
+
+
+p1 <- ggplot(DT1, aes(x=sample, y=Count, fill=name)) +
   geom_bar(stat="identity", position="stack") +
-  scale_x_discrete(position = "top") +
-  scale_fill_manual(values=c("darkgreen", "gold3")) +
+  geom_hline(, yintercept = opt$cnt_thresh, linetype = 'dashed', colour = 'blue') +
+  scale_x_discrete(position = "bottom") +
+  scale_fill_manual(values=fills) +
   theme_bw() +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+  theme(axis.text.x = element_blank(), axis.title.x = element_blank())
+  #theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
 
 p2 <- ggplot(DT2, aes(x=sample, y=PC, fill=name)) +
-  facet_wrap(~ type, nrow = 3, scales = "free_y") +
   geom_bar(stat="identity", position="stack") +
+  geom_hline(yintercept = opt$pc_thresh, linetype = 'dashed', colour = 'blue') +
   scale_x_discrete(position = "top") +
-  scale_fill_manual(values=c("darkgreen", "gold3")) +
+  #scale_y_reverse() +
+  scale_fill_manual(values=fills) +
   theme_bw() +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+  theme(axis.text.x = element_text(angle=90, hjust=0, vjust=0.5))
 
 
-DT3 <- DT2[name == "primary" & type == "unambiguousReadsPC", ]
+
+
+DT3 <- merge(DT1[name == "primary" & type == "unambiguousReads", ],
+             DT2[name == "primary" & type == "unambiguousReadsPC", ],
+             by = c('sample', 'name', 'row'))
+setnames(DT3, c('sample', 'drop0', 'drop1', 'drop2', 'unambiguousReads', 'drop3', 'unambiguousReadsPC'))
+DT3 <- DT3[, !grepl('drop', names(DT3)), with = FALSE]
 setkey(DT3, sample)
 
+
 DT3[, bracket := 0]
-DT3[PC >= 50, bracket := 50]
-DT3[PC >= 30 & PC < 50, bracket := 30]
-DT3[PC >= 20 & PC < 30, bracket := 20]
-DT3[PC >= 15 & PC < 20, bracket := 15]
-DT3[PC >= 5 & PC < 15, bracket := 5]
+brackets <- sort(unique(c(1e9, 15e6, opt$cnt_thresh, 1e6, 0)), decreasing = TRUE)  # include large enough top limit and 0 for conditional to work correctly
+for (i in 2:(length(brackets)) ) {
+  DT3[unambiguousReads > brackets[i] & unambiguousReads <= brackets[i-1], bracket := brackets[i]]
+}
+
+DT3[, bracketPC := 0]
+brackets <- sort(unique(c(100, 50, opt$pc_thresh, 5, 0)), decreasing = TRUE)  # include 100 and 0 for conditional to work correctly
+for (i in 2:(length(brackets)) ) {
+  DT3[unambiguousReadsPC > brackets[i] & unambiguousReadsPC <= brackets[i-1], bracketPC := brackets[i]]
+}
+
 print('Brackets')
 print(table(DT3$bracket))
+print(table(DT3$bracketPC))
 
-DT3[, use := PC > 20]
+DT3[, use := unambiguousReads > opt$cnt_thresh]
 print('Use')
 print(table(DT3$use))
 
-DT1 <- dcast(DT1[name == 'primary', ], sample ~ type, value.var = 'PC')
-DT2 <- dcast(DT2[name == 'primary', ], sample ~ type, value.var = 'PC')
+
+
+
+
+DT1 <- dcast(DT1[name %in% c('primary', 'ambiguous'), ], sample ~ type, value.var = 'Count')
+DT2 <- dcast(DT2[name %in% c('primary', 'ambiguous'), ], sample ~ type, value.var = 'PC')
 
 DT <- merge(DT1, DT2, by='sample')
-DT <- merge(DT, DT3[, .(sample, bracket, use)], by = 'sample')
+DT <- merge(DT, DT3[, .(sample, bracket, bracketPC, use)], by = 'sample')
 
 fwrite(DT, file = file.path(opt$baseDir, opt$bbDir, 'bbsplit.stats.csv'), sep=',', col.names = TRUE)
 
 pdf(file = file.path(opt$baseDir, opt$bbDir, 'bbsplit.stats.pdf'), width = 10, height = 10)
-print(p1)
-print(p2)
+print(p1 / p2)
 dev.off()
